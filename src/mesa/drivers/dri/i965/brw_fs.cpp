a23317952da096b4f462bcc5337cb6f669df8c71
Connor Abbott <connor.abbott@intel.com>
2014-08-15 10:32:07 -0700

    i965/fs: add a NIR frontend
    This is similar to the GLSL IR frontend, except consuming NIR. This lets
    us test NIR as part of an actual compiler.
    
    v2: Jason Ekstrand <jason.ekstrand@intel.com>:
       Make brw_fs_nir build again
       Only use NIR of INTEL_USE_NIR is set
       whitespace fixes

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -3600,10 +3600,14 @@ fs_visitor::run()
        * functions called "main").
        */
       if (shader) {
-         foreach_in_list(ir_instruction, ir, shader->base.ir) {
-            base_ir = ir;
-            this->result = reg_undef;
-            ir->accept(this);
+         if (getenv("INTEL_USE_NIR") != NULL) {
+            emit_nir_code();
+         } else {
+            foreach_in_list(ir_instruction, ir, shader->base.ir) {
+               base_ir = ir;
+               this->result = reg_undef;
+               ir->accept(this);
+            }
          }
       } else {
          emit_fragment_program_code();
