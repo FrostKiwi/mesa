6e76245ae350d3a66042fd689d2fd033830734c0
Jason Ekstrand <jason.ekstrand@intel.com>
2014-08-28 16:32:34 -0700

    SQUASH: i965/fs: Change regs_read to be in hardware registers

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -774,10 +774,7 @@ int
 fs_inst::regs_read(fs_visitor *v, int arg) const
 {
    if (is_tex() && arg == 0 && src[0].file == GRF) {
-      if (v->dispatch_width == 16)
-	 return (mlen + 1) / 2;
-      else
-	 return mlen;
+      return ALIGN(mlen, v->dispatch_width / 8);
    }
    return 1;
 }
