c41b59a221b119a56d52b74e6a4c8de0c08a53c4
Jason Ekstrand <jason.ekstrand@intel.com>
2014-09-11 19:10:14 -0700

    SQUASH: i965/fs: Don't waste a register on texture lookups for gen >= 7
    Previously, we were waisting a register in SIMD16 mode because we could
    only allocate registers in pairs.  Now that we can allocate and address
    odd-sized registers, let's get rid of this special-case.

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -829,7 +829,7 @@ int
 fs_inst::regs_read(fs_visitor *v, int arg) const
 {
    if (is_tex() && arg == 0 && src[0].file == GRF) {
-      return ALIGN(mlen, v->dispatch_width / 8);
+      return mlen;
    }
 
    switch (src[arg].file) {
