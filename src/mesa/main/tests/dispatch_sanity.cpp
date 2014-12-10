639ebd22f027971e9ad79ed5c001ea32497242f6
Laura Ekstrand <laura@jlekstrand.net>
2014-12-10 15:32:20 -0800

    main: Added entry point for glGetTextureParameterfv.

--- src/mesa/main/tests/dispatch_sanity.cpp
+++ src/mesa/main/tests/dispatch_sanity.cpp
@@ -968,6 +968,7 @@ const struct function gl_core_functions_possible[] = {
    { "glTextureParameteriv", 45, -1 },
    { "glGetTextureLevelParameterfv", 45, -1 },
    { "glGetTextureLevelParameteriv", 45, -1 },
+   { "glGetTextureParameterfv", 45, -1 },
 
    { NULL, 0, -1 }
 };
