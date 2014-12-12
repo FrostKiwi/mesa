eed3531acbfa0ce70de1ca1dd6f16bedfe1532f3
Jason Ekstrand <jason.ekstrand@intel.com>
2014-12-12 15:34:21 -0800

    SQUASH: Only use NIR of INTEL_USE_NIR is set

--- src/mesa/drivers/dri/i965/brw_fs.cpp
+++ src/mesa/drivers/dri/i965/brw_fs.cpp
@@ -3600,15 +3600,15 @@ fs_visitor::run()
        * functions called "main").
        */
       if (shader) {
-#if 0
-         foreach_in_list(ir_instruction, ir, shader->base.ir) {
-            base_ir = ir;
-            this->result = reg_undef;
-            ir->accept(this);
+         if (getenv("INTEL_USE_NIR") != NULL) {
+            emit_nir_code();
+         } else {
+            foreach_in_list(ir_instruction, ir, shader->base.ir) {
+               base_ir = ir;
+               this->result = reg_undef;
+               ir->accept(this);
+            }
          }
-#else
-         emit_nir_code();
-#endif
       } else {
          emit_fragment_program_code();
       }
