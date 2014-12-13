3f3cf50c1c67ed3996a83eafa500259727420f24
Jason Ekstrand <jason.ekstrand@intel.com>
2014-12-12 22:38:41 -0800

    nir: Remove predication
    We stopped generating predicates in glsl_to_nir some time ago.  Right now,
    it's all dead untested code that I'm not convinced always worked in the
    first place.  If we decide we want them back, we can revert this patch.

--- src/glsl/nir/nir.c
+++ src/glsl/nir/nir.c
@@ -377,9 +377,6 @@ nir_alu_instr_create(void *mem_ctx, nir_op op)
    for (unsigned i = 0; i < num_srcs; i++)
       alu_src_init(&instr->src[i]);
 
-   instr->has_predicate = false;
-   src_init(&instr->predicate);
-
    return instr;
 }
 
@@ -402,9 +399,6 @@ nir_load_const_instr_create(void *mem_ctx)
    instr->num_components = 0;
    instr->array_elems = 0;
 
-   instr->has_predicate = false;
-   src_init(&instr->predicate);
-
    return instr;
 }
 
@@ -425,9 +419,6 @@ nir_intrinsic_instr_create(void *mem_ctx, nir_intrinsic_op op)
    for (unsigned i = 0; i < num_srcs; i++)
       src_init(&instr->src[i]);
 
-   instr->has_predicate = false;
-   src_init(&instr->predicate);
-
    return instr;
 }
 
@@ -442,9 +433,6 @@ nir_call_instr_create(void *mem_ctx, nir_function_overload *callee)
    instr->params = ralloc_array(mem_ctx, nir_deref_var *, instr->num_params);
    instr->return_deref = NULL;
 
-   instr->has_predicate = false;
-   src_init(&instr->predicate);
-
    return instr;
 }
 
@@ -457,9 +445,6 @@ nir_tex_instr_create(void *mem_ctx, unsigned num_srcs)
    instr->num_srcs = num_srcs;
    for (unsigned i = 0; i < num_srcs; i++)
       src_init(&instr->src[i]);
-
-   instr->has_predicate = false;
-   src_init(&instr->predicate);
 
    instr->sampler_index = 0;
    instr->has_sampler_indirect = false;
@@ -1518,10 +1503,6 @@ visit_alu_src(nir_alu_instr *instr, nir_foreach_src_cb cb, void *state)
       if (!visit_src(&instr->src[i].src, cb, state))
          return false;
 
-   if (instr->has_predicate)
-      if (!visit_src(&instr->predicate, cb, state))
-         return false;
-
    return true;
 }
 
@@ -1530,10 +1511,6 @@ visit_tex_src(nir_tex_instr *instr, nir_foreach_src_cb cb, void *state)
 {
    for (unsigned i = 0; i < instr->num_srcs; i++)
       if (!visit_src(&instr->src[i], cb, state))
-         return false;
-
-   if (instr->has_predicate)
-      if (!visit_src(&instr->predicate, cb, state))
          return false;
 
    if (instr->has_sampler_indirect)
@@ -1562,20 +1539,12 @@ visit_intrinsic_src(nir_intrinsic_instr *instr, nir_foreach_src_cb cb,
       if (!visit_deref_src(instr->variables[i], cb, state))
          return false;
 
-   if (instr->has_predicate)
-      if (!visit_src(&instr->predicate, cb, state))
-         return false;
-
    return true;
 }
 
 static bool
 visit_call_src(nir_call_instr *instr, nir_foreach_src_cb cb, void *state)
 {
-   if (instr->has_predicate)
-      if (!visit_src(&instr->predicate, cb, state))
-         return false;
-
    return true;
 }
 
@@ -1583,10 +1552,6 @@ static bool
 visit_load_const_src(nir_load_const_instr *instr, nir_foreach_src_cb cb,
                      void *state)
 {
-   if (instr->has_predicate)
-      if (!visit_src(&instr->predicate, cb, state))
-         return false;
-
    return true;
 }
 
