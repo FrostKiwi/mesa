f83d752e1b5acadebad7a24e2ba03e13ace20894
Jason Ekstrand <jason.ekstrand@intel.com>
2016-06-03 16:10:20 -0700

    i965/miptree: Add a helper for getting the aux isl_surf from a miptree

--- src/mesa/drivers/dri/i965/intel_mipmap_tree.h
+++ src/mesa/drivers/dri/i965/intel_mipmap_tree.h
@@ -801,6 +801,10 @@ void
 intel_miptree_get_isl_surf(struct brw_context *brw,
                            const struct intel_mipmap_tree *mt,
                            struct isl_surf *surf);
+void
+intel_miptree_get_ccs_isl_surf(struct brw_context *brw,
+                               const struct intel_mipmap_tree *mt,
+                               struct isl_surf *surf);
 
 union isl_color_value
 intel_miptree_get_isl_clear_color(struct brw_context *brw,
