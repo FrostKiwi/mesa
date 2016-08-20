939a93438b7b31b62e3bdfc6440bc508f77c84e2
Jason Ekstrand <jason.ekstrand@intel.com>
2016-08-20 06:11:41 -0700

    anv/pipeline: Roll compute_urb_setup into emit_urb_setup

--- src/intel/vulkan/anv_private.h
+++ src/intel/vulkan/anv_private.h
@@ -1478,9 +1478,6 @@ struct anv_pipeline {
 
    const struct brw_stage_prog_data *           prog_data[MESA_SHADER_STAGES];
    struct {
-      uint32_t                                  start[MESA_SHADER_GEOMETRY + 1];
-      uint32_t                                  size[MESA_SHADER_GEOMETRY + 1];
-      uint32_t                                  entries[MESA_SHADER_GEOMETRY + 1];
       const struct gen_l3_config *              l3_config;
       uint32_t                                  total_size;
    } urb;
