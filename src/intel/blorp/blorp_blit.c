6fc44d6cf083b69cada43c930300e3469bf0d15a
Jason Ekstrand <jason.ekstrand@intel.com>
2016-08-29 09:48:10 -0700

    intel/blorp: Handle 3D surfaces in convert_to_single_slice

--- src/intel/blorp/blorp_blit.c
+++ src/intel/blorp/blorp_blit.c
@@ -1260,10 +1260,15 @@ surf_convert_to_single_slice(const struct isl_device *isl_dev,
        info->surf.levels == 0 && info->surf.logical_level0_px.array_len == 0)
       return;
 
+   uint32_t layer = 0, z = 0;
+   if (info->surf.dim == ISL_SURF_DIM_3D)
+      z = info->view.base_array_layer;
+   else
+      layer = info->view.base_array_layer;
+
    uint32_t x_offset_sa, y_offset_sa;
    isl_surf_get_image_offset_sa(&info->surf, info->view.base_level,
-                                info->view.base_array_layer, 0,
-                                &x_offset_sa, &y_offset_sa);
+                                layer, z, &x_offset_sa, &y_offset_sa);
 
    uint32_t byte_offset;
    isl_tiling_get_intratile_offset_sa(isl_dev, info->surf.tiling,
