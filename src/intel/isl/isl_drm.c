e5327123d31bdd0a2b3eba680846c3ac65d2013c
Jason Ekstrand <jason.ekstrand@intel.com>
2017-06-13 12:06:49 -0700

    intel/isl: Add support for I915_FORMAT_MOD_Y_TILED_CCS

--- src/intel/isl/isl_drm.c
+++ src/intel/isl/isl_drm.c
@@ -52,6 +52,10 @@ isl_tiling_to_i915_tiling(enum isl_tiling tiling)
    }
 }
 
+#ifndef I915_FORMAT_MOD_Y_TILED_CCS
+#define I915_FORMAT_MOD_Y_TILED_CCS fourcc_mod_code(INTEL, 4)
+#endif
+
 struct isl_drm_modifier_info modifier_info[] = {
    {
       .modifier = DRM_FORMAT_MOD_NONE,
@@ -67,6 +71,13 @@ struct isl_drm_modifier_info modifier_info[] = {
       .modifier = I915_FORMAT_MOD_Y_TILED,
       .name = "I915_FORMAT_MOD_Y_TILED",
       .tiling = ISL_TILING_Y0,
+   },
+   {
+      .modifier = I915_FORMAT_MOD_Y_TILED_CCS,
+      .name = "I915_FORMAT_MOD_Y_TILED_CCS",
+      .tiling = ISL_TILING_Y0,
+      .aux_usage = ISL_AUX_USAGE_CCS_E,
+      .supports_clear_color = false,
    },
 };
 
