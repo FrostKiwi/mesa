8f23cb5c1713573d1411739aa1ae60688603dfed
Jason Ekstrand <jason.ekstrand@intel.com>
2017-08-22 13:23:59 -0700

    nir: Add a new subgroups lowering pass
    This commit pulls nir_lower_read_invocations_to_scalar along with most
    of the guts of nir_opt_intrinsics (which mostly does subgroup lowering)
    into a new nir_lower_subgroups pass.  There are various other bits of
    subgroup lowering that we're going to want to do so it makes a bit more
    sense to keep it all together in one pass.  We also move it in i965 to
    happen after nir_lower_system_values to ensure that because we want to
    handle the subgroup mask system value intrinsics here.

--- src/compiler/nir/nir_opt_intrinsics.c
+++ src/compiler/nir/nir_opt_intrinsics.c
@@ -46,22 +46,14 @@ opt_intrinsics_impl(nir_function_impl *impl)
 
          switch (intrin->intrinsic) {
          case nir_intrinsic_vote_any:
-         case nir_intrinsic_vote_all: {
-            nir_const_value *val = nir_src_as_const_value(intrin->src[0]);
-            if (!val && !b.shader->options->lower_vote_trivial)
-               continue;
-
-            replacement = nir_ssa_for_src(&b, intrin->src[0], 1);
+         case nir_intrinsic_vote_all:
+            if (nir_src_as_const_value(intrin->src[0]))
+               replacement = nir_ssa_for_src(&b, intrin->src[0], 1);
             break;
-         }
-         case nir_intrinsic_vote_eq: {
-            nir_const_value *val = nir_src_as_const_value(intrin->src[0]);
-            if (!val && !b.shader->options->lower_vote_trivial)
-               continue;
-
-            replacement = nir_imm_int(&b, NIR_TRUE);
+         case nir_intrinsic_vote_eq:
+            if (nir_src_as_const_value(intrin->src[0]))
+               replacement = nir_imm_int(&b, NIR_TRUE);
             break;
-         }
          case nir_intrinsic_ballot: {
             assert(b.shader->options->max_subgroup_size != 0);
             if (b.shader->options->max_subgroup_size > 32 ||
@@ -78,37 +70,6 @@ opt_intrinsics_impl(nir_function_impl *impl)
             replacement = nir_pack_64_2x32_split(&b,
                                                  &ballot->dest.ssa,
                                                  nir_imm_int(&b, 0));
-            break;
-         }
-         case nir_intrinsic_load_subgroup_eq_mask:
-         case nir_intrinsic_load_subgroup_ge_mask:
-         case nir_intrinsic_load_subgroup_gt_mask:
-         case nir_intrinsic_load_subgroup_le_mask:
-         case nir_intrinsic_load_subgroup_lt_mask: {
-            if (!b.shader->options->lower_subgroup_masks)
-               break;
-
-            nir_ssa_def *count = nir_load_subgroup_invocation(&b);
-
-            switch (intrin->intrinsic) {
-            case nir_intrinsic_load_subgroup_eq_mask:
-               replacement = nir_ishl(&b, nir_imm_int64(&b, 1ull), count);
-               break;
-            case nir_intrinsic_load_subgroup_ge_mask:
-               replacement = nir_ishl(&b, nir_imm_int64(&b, ~0ull), count);
-               break;
-            case nir_intrinsic_load_subgroup_gt_mask:
-               replacement = nir_ishl(&b, nir_imm_int64(&b, ~1ull), count);
-               break;
-            case nir_intrinsic_load_subgroup_le_mask:
-               replacement = nir_inot(&b, nir_ishl(&b, nir_imm_int64(&b, ~1ull), count));
-               break;
-            case nir_intrinsic_load_subgroup_lt_mask:
-               replacement = nir_inot(&b, nir_ishl(&b, nir_imm_int64(&b, ~0ull), count));
-               break;
-            default:
-               unreachable("you seriously can't tell this is unreachable?");
-            }
             break;
          }
          default:
