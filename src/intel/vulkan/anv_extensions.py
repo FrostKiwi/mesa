860b39a18f0523190ae9db50544be193aee9827d
Keith Packard <keithp@keithp.com>
2017-10-11 00:20:08 -0700

    anv: Add VK_EXT_display_surface_counter to anv driver [v4]
    This extension is required to support EXT_display_control as it offers
    a way to query whether the vblank counter is supported.
    
    v4: Add anv support
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/intel/vulkan/anv_extensions.py
+++ src/intel/vulkan/anv_extensions.py
@@ -114,6 +114,7 @@ EXTENSIONS = [
     Extension('VK_EXT_external_memory_dma_buf',           1, True),
     Extension('VK_EXT_global_priority',                   1,
               'device->has_context_priority'),
+    Extension('VK_EXT_display_surface_counter',           1, 'VK_USE_PLATFORM_DISPLAY_KHR'),
 ]
 
 class VkVersion:
