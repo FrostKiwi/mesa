c6c38fa2371d243d87ae4c7fcfa43ea42ff81a2c
Keith Packard <keithp@keithp.com>
2018-02-07 10:31:44 -0800

    anv: Add KHR_display extension to anv [v4]
    This adds support for the KHR_display extension to the anv Vulkan
    driver. The driver now attempts to open the master DRM node when the
    KHR_display extension is requested so that the common winsys code can
    perform the necessary operations.
    
    v2: Make sure primary fd is usable
    
    	When KHR_display is selected, we try to open the primary node
    	instead of the render node in case the user wants to use
    	KHR_display for presentation. However, if we're actually going
    	to end up using RandR leases, then we don't care if the
    	resulting fd can't be used for display, but the kernel also
    	prevents us from using it for drawing when someone else has
    	master.
    
    v3:
    	Simplify addition of VK_USE_PLATFORM_DISPLAY_KHR to vulkan_wsi_args
    
    	Suggested-by: Eric Engestrom <eric.engestrom@imgtec.com>
    
    v4:
    	Adapt primary node usage to new wsi_device_init API
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -278,6 +278,7 @@ anv_physical_device_init_uuids(struct anv_physical_device *device)
 static VkResult
 anv_physical_device_init(struct anv_physical_device *device,
                          struct anv_instance *instance,
+                         const char *primary_path,
                          const char *path)
 {
    VkResult result;
@@ -443,6 +444,25 @@ anv_physical_device_init(struct anv_physical_device *device,
 
    anv_physical_device_get_supported_extensions(device,
                                                 &device->supported_extensions);
+
+   if (instance->enabled_extensions.KHR_display) {
+      master_fd = open(path, O_RDWR | O_CLOEXEC);
+      if (master_fd >= 0) {
+         /* prod the device with a GETPARAM call which will fail if
+          * we don't have permission to even render on this device
+          */
+         drm_i915_getparam_t gp;
+         memset(&gp, '\0', sizeof(gp));
+         int devid = 0;
+         gp.param = I915_PARAM_CHIPSET_ID;
+         gp.value = &devid;
+         int ret = drmIoctl(fd, DRM_IOCTL_I915_GETPARAM, &gp);
+         if (ret < 0) {
+            close(master_fd);
+            master_fd = -1;
+         }
+      }
+   }
 
    device->local_fd = fd;
    device->master_fd = master_fd;
@@ -634,6 +654,7 @@ anv_enumerate_devices(struct anv_instance *instance)
 
          result = anv_physical_device_init(&instance->physicalDevice,
                         instance,
+                        devices[i]->nodes[DRM_NODE_PRIMARY],
                         devices[i]->nodes[DRM_NODE_RENDER]);
          if (result != VK_ERROR_INCOMPATIBLE_DRIVER)
             break;
