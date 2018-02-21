79ca7c32551e84050e3dd920d9db9c5d50011df3
Jason Ekstrand <jason.ekstrand@intel.com>
2018-02-21 02:53:14 -0800

    intel/isl: Make the offset helpers four dimensional
    We need to do this in order to handle Yf and Ys tiling because they use
    a four-dimensional tile instead of laying everything out in two
    dimensions.
    
    Reviewed-by: Topi Pohjolainen <topi.pohjolainen@intel.com>

--- src/mesa/drivers/dri/i965/intel_mipmap_tree.c
+++ src/mesa/drivers/dri/i965/intel_mipmap_tree.c
@@ -1328,7 +1328,7 @@ intel_miptree_get_image_offset(const struct intel_mipmap_tree *mt,
       return;
    }
 
-   uint32_t x_offset_sa, y_offset_sa;
+   uint32_t x_offset_sa, y_offset_sa, z_offset_sa, array_offset;
 
    /* Miptree itself can have an offset only if it represents a single
     * slice in an imported buffer object.
@@ -1346,10 +1346,13 @@ intel_miptree_get_image_offset(const struct intel_mipmap_tree *mt,
    const unsigned z = mt->surf.dim == ISL_SURF_DIM_3D ? slice : 0;
    slice = mt->surf.dim == ISL_SURF_DIM_3D ? 0 : slice;
    isl_surf_get_image_offset_el(&mt->surf, level, slice, z,
-                                &x_offset_sa, &y_offset_sa);
+                                &x_offset_sa, &y_offset_sa,
+                                &z_offset_sa, &array_offset);
 
    *x = x_offset_sa;
    *y = y_offset_sa;
+   assert(z_offset_sa == 0);
+   assert(array_offset == 0);
 }
 
 
