62b46b04f4ecb22dcda476c0ed4d19f079d50ff5
Jason Ekstrand <jason.ekstrand@intel.com>
2018-03-02 15:30:24 -0800

    intel/isl: Add a max_miptail_levels field to isl_tile_info
    Reviewed-by: Topi Pohjolainen <topi.pohjolainen@intel.com>

--- src/intel/isl/isl.c
+++ src/intel/isl/isl.c
@@ -181,6 +181,7 @@ isl_tiling_get_info(enum isl_tiling tiling,
       return;
    }
 
+   uint32_t max_miptail_levels = 0;
    switch (tiling) {
    case ISL_TILING_LINEAR:
       assert(bs > 0);
@@ -239,6 +240,7 @@ isl_tiling_get_info(enum isl_tiling tiling,
             .d = 1,
             .a = 1,
          };
+         max_miptail_levels = is_Ys ? 16 : 12;
          break;
 
       case ISL_SURF_DIM_2D:
@@ -262,6 +264,7 @@ isl_tiling_get_info(enum isl_tiling tiling,
             logical_el.h >>= (ffs(samples) - 1) / 2;
             logical_el.a = samples;
          }
+         max_miptail_levels = is_Ys ? 16 - ffs(samples) : 11;
          break;
 
       case ISL_SURF_DIM_3D:
@@ -279,6 +282,7 @@ isl_tiling_get_info(enum isl_tiling tiling,
             .d = 1 << (4 - ((ffs(format_bpb) - 3) / 3) + (1 * is_Ys)),
             .a = 1,
          };
+         max_miptail_levels = is_Ys ? 16 : 12;
          break;
       }
 
@@ -331,6 +335,7 @@ isl_tiling_get_info(enum isl_tiling tiling,
       .format_bpb = format_bpb,
       .logical_extent_el = logical_el,
       .phys_extent_B = phys_B,
+      .max_miptail_levels = max_miptail_levels,
    };
 }
 
