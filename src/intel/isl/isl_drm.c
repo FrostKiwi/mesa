cba3fa3dca95c79d8980161236f827b2a0a48fd2
Jason Ekstrand <jason.ekstrand@intel.com>
2018-03-06 21:04:50 -0800

    intel/isl: Add gen10 variants of Yf and Ys tiling
    Reviewed-by: Topi Pohjolainen <topi.pohjolainen@intel.com>

--- src/intel/isl/isl_drm.c
+++ src/intel/isl/isl_drm.c
@@ -46,6 +46,8 @@ isl_tiling_to_i915_tiling(enum isl_tiling tiling)
    case ISL_TILING_W:
    case ISL_TILING_GEN9_Yf:
    case ISL_TILING_GEN9_Ys:
+   case ISL_TILING_GEN10_Yf:
+   case ISL_TILING_GEN10_Ys:
    case ISL_TILING_HIZ:
    case ISL_TILING_CCS:
       return I915_TILING_NONE;
