535265e0ceaba5a3d61f01a0c6f684f2b8090e1b
Jason Ekstrand <jason.ekstrand@intel.com>
2018-07-03 21:52:56 -0700

    intel/nir: Enable nir_opt_move_discards_to_top
    According to RenderDoc, this shaves about 15% off of one of the render
    passes in the Skyrim SE frame I've been analyzing on my KBL laptop and
    about 8% off the whole frame.  It's a bit noisy and hard to tell with
    just the DXVK HUD but there appears to be a small FPS bump in-game as
    well.

--- src/intel/compiler/brw_nir.c
+++ src/intel/compiler/brw_nir.c
@@ -663,6 +663,9 @@ brw_preprocess_nir(const struct brw_compiler *compiler, nir_shader *nir)
 
    nir = brw_nir_optimize(nir, compiler, is_scalar);
 
+   if (nir->info.stage == MESA_SHADER_FRAGMENT)
+      OPT(nir_opt_move_discards_to_top);
+
    /* This needs to be run after the first optimization pass but before we
     * lower indirect derefs away
     */
