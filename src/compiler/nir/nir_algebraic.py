ff99f408e1940cd8d0f9e22ed620e0005be9e72e
Jason Ekstrand <jason.ekstrand@intel.com>
2018-11-08 14:00:07 -0600

    nir/algebraic: Clean up some __str__ cruft
    Both of these things are already handled in the Value base class so we
    don't need to handle them explicitly in Constant.

--- src/compiler/nir/nir_algebraic.py
+++ src/compiler/nir/nir_algebraic.py
@@ -136,7 +136,6 @@ class Constant(Value):
    def __init__(self, val, name):
       Value.__init__(self, val, name, "constant")
 
-      self.in_val = str(val)
       if isinstance(val, (str)):
          m = _constant_re.match(val)
          self.value = ast.literal_eval(m.group('value'))
@@ -201,9 +200,6 @@ class Variable(Value):
          assert self.required_type in ('float', 'bool', 'int', 'uint')
 
       self.index = varset[self.var_name]
-
-   def __str__(self):
-      return self.in_val
 
    def type(self):
       if self.required_type == 'bool':
