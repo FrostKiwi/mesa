11645348d965fcc10028611667c002c4f4098cee
Jason Ekstrand <jason.ekstrand@intel.com>
2019-03-21 14:45:33 -0500

    intel/ibc: Add generic bit-sized types with no interpretation

--- src/intel/compiler/ibc_to_binary.c
+++ src/intel/compiler/ibc_to_binary.c
@@ -32,6 +32,10 @@ brw_reg_type_for_ibc_type(enum ibc_type type)
    case IBC_TYPE_INT:
    case IBC_TYPE_UINT:
    case IBC_TYPE_FLOAT:
+   case IBC_TYPE_8_BIT:
+   case IBC_TYPE_16_BIT:
+   case IBC_TYPE_32_BIT:
+   case IBC_TYPE_64_BIT:
       unreachable("Partial IBC type");
 
    case IBC_TYPE_V:  return BRW_REGISTER_TYPE_V;
@@ -148,6 +152,10 @@ brw_reg_for_ibc_alu_src(const struct gen_device_info *devinfo,
    case IBC_TYPE_INT:
    case IBC_TYPE_UINT:
    case IBC_TYPE_FLOAT:
+   case IBC_TYPE_8_BIT:
+   case IBC_TYPE_16_BIT:
+   case IBC_TYPE_32_BIT:
+   case IBC_TYPE_64_BIT:
    case IBC_TYPE_B:
    case IBC_TYPE_UB:
       unreachable("Invalid immediate types");
