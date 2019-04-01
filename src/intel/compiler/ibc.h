70b8d86c48988c539602cf96f7b77ca94a769ed1
Jason Ekstrand <jason@jlekstrand.net>
2019-04-01 14:05:09 -0500

    intel/ibc: Add and use a new SIMD ZIP intrinsic
    This lets us do SIMD lowering but keep stuff SSA so that we can do
    copy-prop on it.  This unlocks piles of opportunity for copy-prop and,
    with it, dead code.

--- src/intel/compiler/ibc.h
+++ src/intel/compiler/ibc.h
@@ -394,7 +394,8 @@ ibc_send_instr *ibc_send_instr_create(struct ibc_shader *shader,
 
 enum ibc_intrinsic_op {
    IBC_INTRINSIC_OP_INVALID,
-   IBC_INTRINSIC_OP_BTI_UNTYPED_WRITE = 1,
+   IBC_INTRINSIC_OP_SIMD_ZIP,
+   IBC_INTRINSIC_OP_BTI_UNTYPED_WRITE,
 };
 
 typedef struct {
