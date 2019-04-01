bd7e7e7a61ce9d65be1a22cf2f917bd44d771665
Jason Ekstrand <jason@jlekstrand.net>
2019-04-01 11:16:51 -0500

    intel/ibc: Add a copy propagation pass

---
+++ src/intel/compiler/ibc_opt_copy_prop.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright © 2018 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "ibc.h"
+
+static bool
+try_copy_prop_reg_ref(ibc_reg_ref *ref, ibc_alu_src *alu_src)
+{
+   if (ref->file != IBC_REG_FILE_LOGICAL)
+      return false;
+
+   if (ref->reg->logical.ssa == NULL)
+      return false;
+
+   switch (ref->reg->logical.ssa->type) {
+   case IBC_INSTR_TYPE_ALU: {
+      ibc_alu_instr *mov = ibc_instr_as_alu(ref->reg->logical.ssa);
+
+      /* Must be a non-saturating MOV */
+      if (mov->op != IBC_ALU_OP_MOV)
+         return false;
+
+      /* The source must also be SSA */
+      if (mov->src[0].ref.file != IBC_REG_FILE_LOGICAL ||
+          mov->src[0].ref.reg->logical.ssa == NULL)
+         return false;
+
+      /* Cannot saturate or type convert */
+      if (mov->src[0].ref.type != mov->dest.ref.type || mov->dest.sat)
+         return false;
+
+      if (alu_src) {
+         if (!alu_src->abs)
+            alu_src->negate = alu_src->negate != mov->src[0].negate;
+         alu_src->abs = alu_src->abs || mov->src[0].abs;
+      } else if (mov->src[0].negate || mov->src[0].abs) {
+         return false;
+      }
+
+      ref->reg = mov->src[0].ref.reg;
+      ref->comp += mov->src[0].ref.comp;
+      ref->byte += mov->src[0].ref.byte;
+
+      return true;
+   }
+
+   default:
+      return false;
+   }
+}
+
+bool
+ibc_opt_copy_prop(ibc_shader *shader)
+{
+   bool progress = false;
+
+   ibc_foreach_block(block, shader) {
+      ibc_foreach_instr_safe(instr, block) {
+
+         /* TODO: Handle predicate flag? */
+
+         switch (instr->type) {
+         case IBC_INSTR_TYPE_ALU: {
+            ibc_alu_instr *alu = ibc_instr_as_alu(instr);
+
+            unsigned num_srcs = 3; /* TODO */
+            for (unsigned i = 0; i < num_srcs; i++) {
+               if (try_copy_prop_reg_ref(&alu->src[i].ref, &alu->src[i]))
+                  progress = true;
+            }
+         }
+
+         case IBC_INSTR_TYPE_SEND:
+            /* TODO */
+            continue;
+
+         case IBC_INSTR_TYPE_INTRINSIC: {
+            ibc_intrinsic_instr *intrin = ibc_instr_as_intrinsic(instr);
+            for (unsigned i = 0; i < intrin->num_srcs; i++) {
+               if (try_copy_prop_reg_ref(&intrin->src[i].ref, NULL))
+                  progress = true;
+            }
+         }
+
+         case IBC_INSTR_TYPE_JUMP:
+            continue;
+         }
+         unreachable("Unsupported IBC instruction type");
+      }
+   }
+
+   return progress;
+}
