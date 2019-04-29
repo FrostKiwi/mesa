ecdf787d7afad2c4b51d93ffb989b9e27acb1729
Jason Ekstrand <jason@jlekstrand.net>
2019-04-29 17:06:50 -0500

    intel/ibc: Add a pass to lower ZIP, VEC, etc.

---
+++ src/intel/compiler/ibc_lower_gather_ops.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright © 2019 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "ibc.h"
+#include "ibc_builder.h"
+
+static unsigned
+reg_ref_stride(const ibc_reg_ref *ref)
+{
+   switch (ref->file) {
+   case IBC_REG_FILE_NONE:
+   case IBC_REG_FILE_IMM:
+      return 0;
+
+   case IBC_REG_FILE_HW_GRF:
+      return ref->hw_grf.stride;
+
+   case IBC_REG_FILE_LOGICAL:
+      /* TODO: Is this correct? */
+      if (ref->reg->logical.bit_size == 1)
+         return 0;
+
+      return ibc_logical_reg_stride(ref->reg);
+
+   case IBC_REG_FILE_FLAG:
+      unreachable("Should not see flags here");
+   }
+
+   unreachable("Unknown register file");
+}
+
+static ibc_reg_ref
+simd_offset_ref(ibc_reg_ref ref, unsigned simd_group_offset)
+{
+   switch (ref.file) {
+   case IBC_REG_FILE_LOGICAL:
+      return ref;
+
+   case IBC_REG_FILE_HW_GRF:
+      if (ref.hw_grf.stride == 0)
+         return ref;
+
+      ref.hw_grf.offset += simd_group_offset * ref.hw_grf.stride;
+      return ref;
+
+   default:
+      unreachable("Unhandled register file");
+   }
+}
+
+static void
+build_MOV_raw(ibc_builder *b, ibc_reg_ref dest, ibc_reg_ref src)
+{
+   const unsigned total_simd_width = b->simd_width;
+   unsigned split_simd_width = total_simd_width;
+
+   /* Can't span more than two registers
+    *
+    * TODO: Unify with lower_simd_width
+    */
+   const unsigned src_stride = reg_ref_stride(&src);
+   if (src_stride > 0)
+      split_simd_width = MIN2(split_simd_width, 64 / src_stride);
+   const unsigned dest_stride = reg_ref_stride(&dest);
+   if (dest_stride > 0)
+      split_simd_width = MIN2(split_simd_width, 64 / dest_stride);
+
+   assert(src.type == dest.type);
+   assert(ibc_type_base_type(src.type) == IBC_TYPE_INVALID);
+   src.type |= IBC_TYPE_UINT;
+   dest.type |= IBC_TYPE_UINT;
+
+   for (unsigned g = 0; g < total_simd_width; g += split_simd_width) {
+      ibc_builder_push_group(b, g, split_simd_width);
+      ibc_build_alu1(b, IBC_ALU_OP_MOV, dest, src);
+      ibc_builder_pop(b);
+   }
+}
+
+/** Lowers gather operations like SIMD ZIP, VEC, etc. */
+bool
+ibc_lower_gather_ops(ibc_shader *shader)
+{
+   bool progress = false;
+
+   ibc_builder b;
+   ibc_builder_init(&b, shader, 32);
+
+   ibc_foreach_block(block, shader) {
+      ibc_foreach_instr_safe(instr, block) {
+         if (instr->type != IBC_INSTR_TYPE_INTRINSIC)
+            continue;
+
+         b.cursor = ibc_after_instr(instr);
+
+         ibc_intrinsic_instr *intrin = ibc_instr_as_intrinsic(instr);
+         switch (intrin->op) {
+         case IBC_INTRINSIC_OP_SIMD_ZIP:
+            for (unsigned i = 0; i < intrin->num_srcs; i++) {
+               const unsigned rel_group = intrin->src[i].simd_group -
+                                          instr->simd_group;
+               ibc_builder_push_group(&b, rel_group, intrin->src[i].simd_width);
+               assert(intrin->src[i].num_comps == 1); /* TODO */
+               build_MOV_raw(&b, simd_offset_ref(intrin->dest, rel_group),
+                             simd_offset_ref(intrin->src[i].ref, rel_group));
+               ibc_builder_pop(&b);
+            }
+            break;
+         default:
+            continue;
+         }
+
+         ibc_instr_remove(instr);
+         progress = true;
+      }
+   }
+
+   return progress;
+}
