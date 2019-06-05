235702aaca171460003e96ec9afeab18169cbeb5
Jason Ekstrand <jason@jlekstrand.net>
2019-06-05 14:56:20 -0500

    intel/ibc: Add a linear-scan binpacking register allocator
    We don't yet support spilling but this is good enough to pass the first
    1100 dEQP-VK SSBO tests.  There are likely doing to be places where
    we'll want to tune the heuristics but it's been proven to at least work
    which is pretty good.

--- src/intel/compiler/ibc.h
+++ src/intel/compiler/ibc.h
@@ -822,6 +822,7 @@ ibc_shader *nir_to_ibc(const struct nir_shader *nir, void *mem_ctx,
 void ibc_assign_and_lower_flags(ibc_shader *shader);
 void ibc_assign_logical_reg_strides(ibc_shader *shader);
 void ibc_assign_regs(ibc_shader *shader);
+void ibc_assign_regs_trivial(ibc_shader *shader);
 
 bool ibc_lower_gather_ops(ibc_shader *shader);
 bool ibc_lower_phis(ibc_shader *shader);
