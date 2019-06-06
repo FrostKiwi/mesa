28d59cd4d5cc5d31969a2aafbacdcea8e3c50b48
Jason Ekstrand <jason@jlekstrand.net>
2019-06-06 16:39:19 -0500

    HACK: intel/nir: Disable nir_lower_to_source_mods

--- src/intel/compiler/brw_nir.c
+++ src/intel/compiler/brw_nir.c
@@ -872,7 +872,7 @@ brw_postprocess_nir(nir_shader *nir, const struct brw_compiler *compiler,
 
    if (is_scalar)
       OPT(nir_lower_alu_to_scalar, NULL);
-   OPT(nir_lower_to_source_mods, nir_lower_all_source_mods);
+   //OPT(nir_lower_to_source_mods, nir_lower_all_source_mods);
    OPT(nir_copy_prop);
    OPT(nir_opt_dce);
    OPT(nir_opt_move, nir_move_comparisons);
