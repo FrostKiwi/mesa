d73a952917691a7e0ca9a2a86e5cdd66a2532184
Jason Ekstrand <jason@jlekstrand.net>
2019-07-16 19:12:12 -0500

    intel/ibc: Add a LOAD_PAYLOAD intrinsic for loading thread payloads

--- src/intel/compiler/ibc_print.c
+++ src/intel/compiler/ibc_print.c
@@ -345,6 +345,7 @@ intrinsic_op_name(enum ibc_intrinsic_op op)
    case IBC_INTRINSIC_OP_SIMD_ZIP:           return "simd_zip";
    case IBC_INTRINSIC_OP_BTI_UNTYPED_READ:   return "bti_untyped_read";
    case IBC_INTRINSIC_OP_BTI_UNTYPED_WRITE:  return "bti_untyped_write";
+   case IBC_INTRINSIC_OP_LOAD_PAYLOAD:       return "load_payload";
    }
    unreachable("Invalid IBC intrinsic op");
 }
