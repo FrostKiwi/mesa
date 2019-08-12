1d4ac65f0526724a0e17ba5bffbce88582e276f6
Erik Faye-Lund <erik.faye-lund@collabora.com>
2019-08-12 19:41:56 +0200

    HACK: d3d12: implement somewhat working clears
    This is rather messy, but kinda works. We can now do clears, so yay :)

--- src/gallium/drivers/d3d12/d3d12_surface.cpp
+++ src/gallium/drivers/d3d12/d3d12_surface.cpp
@@ -21,31 +21,52 @@
  * USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#include "d3d12_context.h"
+#include "d3d12_resource.h"
+#include "d3d12_screen.h"
 #include "d3d12_surface.h"
 
+#include "util/format/u_format.h"
 #include "util/u_inlines.h"
 #include "util/u_memory.h"
 
 static struct pipe_surface *
-d3d12_create_surface(struct pipe_context *context,
-                     struct pipe_resource *resource,
-                     const struct pipe_surface *surf_tpl)
+d3d12_create_surface(struct pipe_context *pctx,
+                     struct pipe_resource *pres,
+                     const struct pipe_surface *tpl)
 {
-   unsigned int level = surf_tpl->u.tex.level;
+   struct d3d12_resource *res = d3d12_resource(pres);
+   struct d3d12_context *ctx = d3d12_context(pctx);
+   struct d3d12_screen *screen = d3d12_screen(pctx->screen);
+   unsigned int level = tpl->u.tex.level;
 
    struct d3d12_surface *surface = CALLOC_STRUCT(d3d12_surface);
    if (!surface)
       return NULL;
 
-   pipe_resource_reference(&surface->base.texture, resource);
+   pipe_resource_reference(&surface->base.texture, pres);
    pipe_reference_init(&surface->base.reference, 1);
-   surface->base.context = context;
-   surface->base.format = surf_tpl->format;
-   surface->base.width = u_minify(resource->width0, level);
-   surface->base.height = u_minify(resource->height0, level);
+   surface->base.context = pctx;
+   surface->base.format = tpl->format;
+   surface->base.width = u_minify(pres->width0, level);
+   surface->base.height = u_minify(pres->height0, level);
    surface->base.u.tex.level = level;
-   surface->base.u.tex.first_layer = surf_tpl->u.tex.first_layer;
-   surface->base.u.tex.last_layer = surf_tpl->u.tex.last_layer;
+   surface->base.u.tex.first_layer = tpl->u.tex.first_layer;
+   surface->base.u.tex.last_layer = tpl->u.tex.last_layer;
+
+   if (util_format_is_depth_or_stencil(tpl->format)) {
+      surface->desc_handle = ctx->dsv_heap->GetCPUDescriptorHandleForHeapStart();
+      surface->desc_handle.ptr += (size_t)ctx->dsv_increment * ctx->dsv_index;
+      screen->dev->CreateDepthStencilView(res->res, NULL,
+                                          surface->desc_handle);
+      ctx->dsv_index++;
+   } else {
+      surface->desc_handle = ctx->rtv_heap->GetCPUDescriptorHandleForHeapStart();
+      surface->desc_handle.ptr += (size_t)ctx->rtv_increment * ctx->rtv_index;
+      screen->dev->CreateRenderTargetView(res->res, NULL,
+                                          surface->desc_handle);
+      ctx->rtv_index++;
+   }
 
    return &surface->base;
 }
