59dc4b600dcd7c043eca940460cc18f51edb9256
Jason Ekstrand <jason@jlekstrand.net>
2019-08-16 10:58:48 -0500

    HACK: Disable IBC for shaders with push constants or images
    This should avoid a lot of crashing and make CTS runs faster.

--- src/intel/compiler/brw_fs.cpp
+++ src/intel/compiler/brw_fs.cpp
@@ -8097,15 +8097,12 @@ brw_compile_fs(const struct brw_compiler *compiler, void *log_data,
    calculate_urb_setup(devinfo, key, prog_data, shader);
    brw_compute_flat_inputs(prog_data, shader);
 
-   if (!use_rep_send && allow_spilling) {
-      assert(brw_nir_should_use_ibc(shader, compiler, true));
+   if (!use_rep_send && allow_spilling &&
+       brw_nir_should_use_ibc(shader, compiler, true)) {
       return ibc_compile_fs(compiler, log_data, mem_ctx, key, prog_data,
                             shader, prog, shader_time_index8,
                             shader_time_index16, shader_time_index32,
                             allow_spilling, use_rep_send, vue_map, error_str);
-   } else {
-      assert(use_rep_send ||
-             !brw_nir_should_use_ibc(shader, compiler, true));
    }
 
    cfg_t *simd8_cfg = NULL, *simd16_cfg = NULL, *simd32_cfg = NULL;
@@ -8365,9 +8362,10 @@ brw_compile_cs(const struct brw_compiler *compiler, void *log_data,
    assert(min_dispatch_width <= 32);
    unsigned max_dispatch_width = 32;
 
-   assert(brw_nir_should_use_ibc(src_shader, compiler, true));
-   return ibc_compile_cs(compiler, log_data, mem_ctx, key, prog_data,
-                         src_shader, shader_time_index, error_str);
+   if (brw_nir_should_use_ibc(src_shader, compiler, true)) {
+      return ibc_compile_cs(compiler, log_data, mem_ctx, key, prog_data,
+                            src_shader, shader_time_index, error_str);
+   }
 
    fs_visitor *v8 = NULL, *v16 = NULL, *v32 = NULL;
    fs_visitor *v = NULL;
