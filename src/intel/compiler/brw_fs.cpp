0b489424af4ac9313104614aa3e736c03669481f
Jason Ekstrand <jason@jlekstrand.net>
2019-08-16 13:08:58 -0500

    HACK: Call ibc_compile_fs from brw_compile_fs immediately
    All of the NIR lowering and prog data setup is handled from
    ibc_compile_fs so there's no need to call it any later.  This gets rid
    of a bug where brw_postprocess_nir was getting called twice.  The only
    reason why we didn't notice was because we weren't going out-of-SSA.

--- src/intel/compiler/brw_fs.cpp
+++ src/intel/compiler/brw_fs.cpp
@@ -8965,6 +8965,15 @@ brw_compile_fs(const struct brw_compiler *compiler, void *log_data,
    prog_data->base.stage = MESA_SHADER_FRAGMENT;
 
    const struct gen_device_info *devinfo = compiler->devinfo;
+
+   if (!use_rep_send && allow_spilling &&
+       brw_nir_should_use_ibc(shader, compiler, true)) {
+      return ibc_compile_fs(compiler, log_data, mem_ctx, key, prog_data,
+                            shader, shader_time_index8,
+                            shader_time_index16, shader_time_index32,
+                            allow_spilling, use_rep_send, vue_map, error_str);
+   }
+
    const unsigned max_subgroup_size = compiler->devinfo->gen >= 6 ? 32 : 16;
 
    brw_nir_apply_key(nir, compiler, &key->base, max_subgroup_size, true);
@@ -8993,14 +9002,6 @@ brw_compile_fs(const struct brw_compiler *compiler, void *log_data,
    brw_postprocess_nir(nir, compiler, true);
 
    brw_nir_populate_wm_prog_data(nir, compiler->devinfo, key, prog_data);
-
-   if (!use_rep_send && allow_spilling &&
-       brw_nir_should_use_ibc(shader, compiler, true)) {
-      return ibc_compile_fs(compiler, log_data, mem_ctx, key, prog_data,
-                            shader, shader_time_index8,
-                            shader_time_index16, shader_time_index32,
-                            allow_spilling, use_rep_send, vue_map, error_str);
-   }
 
    fs_visitor *v8 = NULL, *v16 = NULL, *v32 = NULL;
    cfg_t *simd8_cfg = NULL, *simd16_cfg = NULL, *simd32_cfg = NULL;
