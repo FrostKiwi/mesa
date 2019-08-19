e1e55a6ab4b447100e9d97354adb4fc2f276d466
Jason Ekstrand <jason@jlekstrand.net>
2019-08-19 14:35:41 -0500

    intel/ibc: Add a stage field to ibc_shader
    Initially, I didn't want to do this because I wanted shaders to stand
    alone and wanted to strongly discourage stage-specific stuff in common
    code.  However, there are some places where it's very useful and one of
    them is in doing good logging based in INTEL_DEBUG.

--- src/intel/compiler/ibc_fragment.c
+++ src/intel/compiler/ibc_fragment.c
@@ -573,7 +573,7 @@ ibc_compile_fs(const struct brw_compiler *compiler, void *log_data,
    }
    ibc_validate_shader(ibc);
 
-   ibc_lower_and_optimize(ibc, INTEL_DEBUG & DEBUG_WM);
+   ibc_lower_and_optimize(ibc);
 
    switch (simd_width) {
    case 8:
@@ -592,6 +592,5 @@ ibc_compile_fs(const struct brw_compiler *compiler, void *log_data,
       unreachable("Invalid dispatch width");
    }
 
-   return ibc_to_binary(ibc, mem_ctx, &prog_data->base.program_size,
-                        INTEL_DEBUG & DEBUG_WM);
+   return ibc_to_binary(ibc, mem_ctx, &prog_data->base.program_size);
 }
