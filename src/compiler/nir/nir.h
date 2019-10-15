de2a316a1d0d1d37d0935fb23c29277dbf640f5f
Jason Ekstrand <jason@jlekstrand.net>
2019-10-15 16:14:39 -0500

    nir/from_ssa: Respect and populate divergence information
    The Intel backend still goes out-of-SSA for values tangled in phi webs,
    but would like to start using divergence information.

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -702,6 +702,12 @@ typedef struct nir_register {
    /* The bit-size of each channel; must be one of 8, 16, 32, or 64 */
    uint8_t bit_size;
 
+   /**
+    * True if this register may have different values in different SIMD
+    * invocations of the shader.
+    */
+   bool divergent;
+
    /** generic register index. */
    unsigned index;
 
@@ -954,8 +960,7 @@ nir_src_is_const(nir_src src)
 static inline bool
 nir_src_is_divergent(nir_src src)
 {
-   assert(src.is_ssa);
-   return src.ssa->divergent;
+   return src.is_ssa ? src.ssa->divergent : src.reg.reg->divergent;
 }
 
 static inline unsigned
@@ -973,8 +978,7 @@ nir_dest_num_components(nir_dest dest)
 static inline bool
 nir_dest_is_divergent(nir_dest dest)
 {
-   assert(dest.is_ssa);
-   return dest.ssa.divergent;
+   return dest.is_ssa ? dest.ssa.divergent : dest.reg.reg->divergent;
 }
 
 /* Are all components the same, ie. .xxxx */
