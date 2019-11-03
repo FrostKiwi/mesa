c359be22193c7408b0aea23c31a3f2173c6c7cc8
Jason Ekstrand <jason@jlekstrand.net>
2019-11-03 10:05:18 -0600

    intel/ibc: Try to coalesce in lower_gather_ops
    Shader-db results on Kaby Lake:
    
        total instructions in shared programs: 23433122 -> 21744968 (-7.20%)
        instructions in affected programs: 22748703 -> 21060549 (-7.42%)
        helped: 154339
        HURT: 1
        helped stats (abs) min: 1 max: 298 x̄: 10.94 x̃: 8
        helped stats (rel) min: 0.09% max: 44.44% x̄: 11.31% x̃: 9.47%
        HURT stats (abs)   min: 6 max: 6 x̄: 6.00 x̃: 6
        HURT stats (rel)   min: 1.26% max: 1.26% x̄: 1.26% x̃: 1.26%
        95% mean confidence interval for instructions value: -11.00 -10.87
        95% mean confidence interval for instructions %-change: -11.35% -11.27%
        Instructions are helped.
    
        total cycles in shared programs: 123523548 -> 117537850 (-4.85%)
        cycles in affected programs: 118700780 -> 112715082 (-5.04%)
        helped: 150998
        HURT: 2600
        helped stats (abs) min: 2 max: 131058 x̄: 42.34 x̃: 22
        helped stats (rel) min: <.01% max: 96.77% x̄: 7.09% x̃: 5.65%
        HURT stats (abs)   min: 2 max: 13962 x̄: 157.04 x̃: 82
        HURT stats (rel)   min: 0.02% max: 994.44% x̄: 8.94% x̃: 5.65%
        95% mean confidence interval for cycles value: -43.38 -34.56
        95% mean confidence interval for cycles %-change: -6.86% -6.79%
        Cycles are helped.
    
        LOST:   837
        GAINED: 113
    
    Of the lost programs, 6 are SIMD8 programs that failed to spill.  They
    were all in Unigen Heaven/Valley and Deus Ex: Mankind Divided.

--- src/intel/compiler/ibc_lower_gather_ops.c
+++ src/intel/compiler/ibc_lower_gather_ops.c
@@ -24,6 +24,226 @@
 #include "ibc.h"
 #include "ibc_builder.h"
 
+#define INVALID_BLOCK_START ((ibc_flow_instr *)1)
+
+struct reg_info {
+   unsigned num_reads;
+   ibc_flow_instr *write_block_start;
+};
+
+struct lower_gather_ops_state {
+   ibc_flow_instr *block_start;
+
+   unsigned num_regs;
+   struct reg_info *reg_info;
+};
+
+static bool
+count_refs(ibc_ref *ref,
+           UNUSED int num_bytes, UNUSED int num_comps,
+           UNUSED uint8_t simd_group, UNUSED uint8_t simd_width,
+           void *_state)
+{
+   struct lower_gather_ops_state *state = _state;
+
+   if (ref->file == IBC_FILE_NONE || ref->file == IBC_FILE_IMM)
+      return true;
+
+   if (ref->reg == NULL)
+      return true;
+
+   struct reg_info *reg_info = &state->reg_info[ref->reg->index];
+
+   reg_info->num_reads++;
+
+   return true;
+}
+
+static bool
+record_write(ibc_ref *ref,
+             UNUSED int num_bytes, UNUSED int num_comps,
+             UNUSED uint8_t simd_group, UNUSED uint8_t simd_width,
+             void *_state)
+{
+   struct lower_gather_ops_state *state = _state;
+
+   if (ref->file == IBC_FILE_NONE || ref->file == IBC_FILE_IMM)
+      return true;
+
+   if (ref->reg == NULL)
+      return true;
+
+   struct reg_info *reg_info = &state->reg_info[ref->reg->index];
+
+   if (reg_info->write_block_start == NULL) {
+      reg_info->write_block_start = state->block_start;
+   } else if (reg_info->write_block_start != state->block_start) {
+      reg_info->write_block_start = INVALID_BLOCK_START;
+   }
+
+   return true;
+}
+
+static bool
+ibc_instr_is_load_payload(const ibc_instr *instr)
+{
+   if (instr->type != IBC_INSTR_TYPE_INTRINSIC)
+      return false;
+
+   ibc_intrinsic_instr *intrin = ibc_instr_as_intrinsic(instr);
+   return intrin->op == IBC_INTRINSIC_OP_LOAD_PAYLOAD ||
+          (intrin->op == IBC_INTRINSIC_OP_BTI_BLOCK_LOAD_UBO &&
+           intrin->src[0].ref.file != IBC_FILE_NONE);
+}
+
+struct rewrite_ref_state {
+   const ibc_reg *src_reg;
+   ibc_ref dest;
+};
+
+static bool
+rewrite_write(ibc_reg_write *write,
+              ibc_ref *ref, void *_state)
+{
+   struct rewrite_ref_state *state = _state;
+
+   if (ref->reg == state->src_reg) {
+      ibc_ref new_ref = state->dest;
+      new_ref.type = ref->type;
+      ibc_instr_set_ref(write->instr, ref, new_ref);
+   }
+
+   return true;
+}
+
+/* We have to split the actual coalesce attempt from the initial check for if
+ * the destination is coalescable because the destination check involves
+ * ibc_reg_ssa_instr and the process of coalescing adds writes which suddenly
+ * makes the destination register not SSA.
+ */
+static bool
+dest_can_try_coalesce(ibc_ref dest)
+{
+   /* For now, coalesce only works on logical and HW_GRF */
+   if (dest.file != IBC_FILE_LOGICAL && dest.file != IBC_FILE_HW_GRF)
+      return false;
+
+   /* Destination needs to be SSA */
+   if (dest.reg == NULL || ibc_reg_ssa_instr(dest.reg) == NULL)
+      return false;
+
+   return true;
+}
+
+static bool
+try_coalesce(ibc_ref dest, ibc_intrinsic_src src,
+             struct lower_gather_ops_state *state)
+{
+   /* For now, this only works on logical */
+   if (src.ref.file != IBC_FILE_LOGICAL)
+      return false;
+
+   /* If nothing writes this register, we can trivially coalesce */
+   if (list_is_empty(&src.ref.reg->writes))
+      return true;
+
+   /* Source needs to be SSA */
+   ibc_instr *ssa_instr = ibc_reg_ssa_instr(src.ref.reg);
+   if (ssa_instr == NULL)
+      return false;
+
+   /* We don't want to coalesce into LOAD_PAYLOAD and BTI_BLOCK_LOAD_UBO
+    * instructions because RA is going to make them no-ops.
+    */
+   if (ibc_instr_is_load_payload(ssa_instr))
+      return false;
+
+   const struct reg_info *dest_info = &state->reg_info[dest.reg->index];
+   const struct reg_info *src_info = &state->reg_info[src.ref.reg->index];
+
+   /* We need to be the only use of this source
+    *
+    * TODO: This isn't strictly required but if it's true it makes everything
+    * really easy so we'll go with it for now.
+    */
+   assert(src_info->num_reads > 0);
+   if (src_info->num_reads != 1)
+      return false;
+
+   /* For now, we want to ensure that the instructions are in the same block.
+    * We could, in theory, handle cross-block coalescing but that shouldn't be
+    * common, will wreak havoc on RA, and breaks WLR.
+    */
+   assert(dest_info->write_block_start != INVALID_BLOCK_START);
+   assert(src_info->write_block_start != INVALID_BLOCK_START);
+   if (dest_info->write_block_start != src_info->write_block_start)
+      return false;
+
+   /* Our source has to read the whole register */
+   if (src.ref.logical.byte > 0 ||
+       src.ref.logical.comp > 0 ||
+       src.ref.logical.broadcast ||
+       ibc_type_bit_size(src.ref.type) != src.ref.reg->logical.bit_size ||
+       src.num_comps != src.ref.reg->logical.num_comps ||
+       src.simd_group != src.ref.reg->logical.simd_group ||
+       src.simd_width != src.ref.reg->logical.simd_width)
+      return false;
+
+   switch (dest.file) {
+   case IBC_FILE_LOGICAL:
+      if (dest.reg->logical.stride != src.ref.reg->logical.stride)
+         return false;
+
+      /* If the source is packed then it needs to either have a single
+       * component or else have the same SIMD group as the destination so that
+       * it can be packed the same way.
+       */
+      if (src.ref.reg->logical.packed &&
+          src.ref.reg->logical.num_comps > 1 &&
+          (src.ref.reg->logical.simd_group != dest.reg->logical.simd_group ||
+           src.ref.reg->logical.simd_width != dest.reg->logical.simd_width))
+         return false;
+
+      if (src.ref.reg->logical.align > 0) {
+         if (dest.reg->logical.align < src.ref.reg->logical.align)
+            return false;
+
+         if (dest.logical.byte > 0)
+            return false;
+      }
+
+      break;
+
+   case IBC_FILE_HW_GRF:
+      /* We have to have a simple stride on the destination */
+      if (dest.hw_grf.vstride != dest.hw_grf.hstride * dest.hw_grf.width)
+         return false;
+
+      /* And it has to match the source */
+      if (dest.hw_grf.hstride != src.ref.reg->logical.stride)
+         return false;
+
+      if (src.ref.reg->logical.align > 0) {
+         if (dest.reg->hw_grf.align < src.ref.reg->logical.align)
+            return false;
+
+         if (dest.hw_grf.byte % src.ref.reg->logical.align != 0)
+            return false;
+      }
+      break;
+
+   default:
+      unreachable("Unhandled register file");
+   }
+
+   ibc_instr_foreach_reg_write(ssa_instr, rewrite_write,
+                               &(struct rewrite_ref_state) {
+                                  .src_reg = src.ref.reg,
+                                  .dest = dest,
+                               });
+   return true;
+}
+
 static unsigned
 ref_stride(const ibc_ref *ref)
 {
@@ -85,38 +305,66 @@ ibc_lower_gather_ops(ibc_shader *shader)
 
    ibc_assign_logical_reg_strides(shader);
 
+   struct lower_gather_ops_state state = { };
+
+   ibc_foreach_reg(reg, shader)
+      reg->index = state.num_regs++;
+
+   state.reg_info = rzalloc_array(shader, struct reg_info, state.num_regs);
+
+   ibc_foreach_instr(instr, shader) {
+      ibc_instr_foreach_read(instr, count_refs, &state);
+      ibc_instr_foreach_write(instr, record_write, &state);
+
+      if (instr->type == IBC_INSTR_TYPE_FLOW)
+         state.block_start = ibc_instr_as_flow(instr);
+   }
+
    ibc_builder b;
    ibc_builder_init(&b, shader);
 
    ibc_foreach_instr_safe(instr, shader) {
+      if (instr->type == IBC_INSTR_TYPE_FLOW) {
+         state.block_start = ibc_instr_as_flow(instr);
+         continue;
+      }
+
       if (instr->type != IBC_INSTR_TYPE_INTRINSIC)
          continue;
 
       b.cursor = ibc_after_instr(instr);
 
       ibc_intrinsic_instr *intrin = ibc_instr_as_intrinsic(instr);
+      bool can_try_coalesce = dest_can_try_coalesce(intrin->dest);
+
       switch (intrin->op) {
       case IBC_INTRINSIC_OP_SIMD_ZIP:
          for (unsigned i = 0; i < intrin->num_srcs; i++) {
-            const unsigned rel_group = intrin->src[i].simd_group -
-                                       instr->simd_group;
-            ibc_builder_push_group(&b, intrin->src[i].simd_group,
-                                       intrin->src[i].simd_width);
-            assert(b.simd_group == intrin->src[i].simd_group);
             assert(intrin->src[i].num_comps == intrin->num_dest_comps);
             if (intrin->src[0].num_comps > 1) {
                assert(intrin->src[i].ref.file == IBC_FILE_IMM ||
                       intrin->src[i].ref.file == IBC_FILE_LOGICAL);
                assert(intrin->dest.file == IBC_FILE_LOGICAL);
             }
-            ibc_ref mov_dest = intrin->dest;
+
+            const unsigned rel_group = intrin->src[i].simd_group -
+                                       instr->simd_group;
+            ibc_ref dest = intrin->dest;
+            ibc_ref_simd_slice(&dest, rel_group);
+
+            if (can_try_coalesce &&
+                try_coalesce(dest, intrin->src[i], &state))
+               continue;
+
+            ibc_builder_push_group(&b, intrin->src[i].simd_group,
+                                       intrin->src[i].simd_width);
+            assert(b.simd_group == intrin->src[i].simd_group);
             ibc_ref mov_src = intrin->src[i].ref;
-            ibc_ref_simd_slice(&mov_dest, rel_group);
             for (unsigned j = 0; j < intrin->src[i].num_comps; j++) {
-               build_MOV_raw(&b, mov_dest, mov_src);
+               build_MOV_raw(&b, dest, mov_src);
                if (mov_src.file == IBC_FILE_LOGICAL)
                   mov_src.logical.comp++;
-               mov_dest.logical.comp++;
+               dest.logical.comp++;
             }
             ibc_builder_pop(&b);
          }
@@ -130,8 +378,11 @@ ibc_lower_gather_ops(ibc_shader *shader)
          ibc_ref dest = intrin->dest;
          for (unsigned i = 0; i < intrin->num_srcs; i++) {
             dest.type = intrin->src[i].ref.type;
-            ibc_MOV_raw_vec_to(&b, dest, intrin->src[i].ref,
-                               intrin->num_dest_comps);
+            if (!can_try_coalesce ||
+                !try_coalesce(dest, intrin->src[i], &state)) {
+               ibc_MOV_raw_vec_to(&b, dest, intrin->src[i].ref,
+                                  intrin->num_dest_comps);
+            }
             dest.logical.byte += ibc_type_byte_size(intrin->src[i].ref.type);
          }
 
@@ -147,7 +398,10 @@ ibc_lower_gather_ops(ibc_shader *shader)
 
          ibc_ref dest = intrin->dest;
          for (unsigned i = 0; i < intrin->num_srcs; i++) {
-            build_MOV_raw(&b, dest, intrin->src[i].ref);
+            if (!can_try_coalesce ||
+                !try_coalesce(dest, intrin->src[i], &state)) {
+               build_MOV_raw(&b, dest, intrin->src[i].ref);
+            }
             dest.logical.comp++;
          }
 
@@ -175,7 +429,9 @@ ibc_lower_gather_ops(ibc_shader *shader)
             dest.hw_grf.width = 8;
             dest.hw_grf.hstride = ibc_type_byte_size(dest.type);
 
-            if (intrin->src[i].ref.file != IBC_FILE_NONE)
+            if (intrin->src[i].ref.file != IBC_FILE_NONE &&
+                (!can_try_coalesce ||
+                 !try_coalesce(dest, intrin->src[i], &state)))
                ibc_MOV_raw_vec_to(&b, dest, intrin->src[i].ref, 1);
 
             unsigned src_bytes = ibc_type_byte_size(intrin->src[i].ref.type) *
@@ -196,6 +452,11 @@ ibc_lower_gather_ops(ibc_shader *shader)
       ibc_instr_remove(instr);
       progress = true;
    }
+
+   if (progress)
+      ibc_repair_wlr_order(shader);
+
+   ralloc_free(state.reg_info);
 
    return progress;
 }
