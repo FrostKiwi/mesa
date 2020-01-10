43bba1a6591b81ec45441009581716190a561545
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-01-10 18:55:45 +0100

    compiler/nir: support lowering not to xor

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -2794,6 +2794,8 @@ typedef struct nir_shader_compiler_options {
    bool lower_negate;
    /** lowers fsub and isub to fadd+fneg and iadd+ineg. */
    bool lower_sub;
+   /** lowers inot to ixor. */
+   bool lower_inot;
 
    /* lower {slt,sge,seq,sne} to {flt,fge,feq,fne} + b2f: */
    bool lower_scmp;
