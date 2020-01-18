d3535d1d722281b5d47a92ef5a7f0dcc60e29208
Louis-Francis Ratté-Boulianne <lfrb@collabora.com>
2020-01-18 01:25:41 -0500

    d3d12: Add debug environment variable

---
+++ src/gallium/drivers/d3d12/d3d12_debug.h
@@ -0,0 +1,18 @@
+#ifndef D3D12_DEBUG_H
+#define D3D12_DEBUG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#define D3D12_DEBUG_VERBOSE 1
+
+extern uint32_t d3d12_debug;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
