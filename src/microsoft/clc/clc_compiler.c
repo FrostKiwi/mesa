9805e445d8136f5609973c8c2aefd33f449e513e
Daniel Stone <daniels@collabora.com>
2020-01-21 23:48:00 +0000

    CLC: Create NIR passes to rewrite memory model to SSBO
    Our backend does not yet support pointer accesses; global memory is
    bound as an SSBO.
    
    When compiling OpenCL source through LLVM to SPIR-V, the only available
    memory model is pointer-based (ShaderInput); SSBO usage is not available
    in this pipeline.
    
    Rather than attempting to modify LLVM, we instead work around this by
    translating pointer-based memory parameters into SSBOs, and the
    corresponding store operations to SSBO stores. This requires working
    backwards from pointers to binding/index; as we only have a single
    pointer, we store the location in the high bits, and hope that no-one
    ever wants more than 15 pointer parameters to a kernel, or more than
    28-bit access locations, or a pointer to a buffer larger than 256MB.
    
    These passes will be dropped once we have a backend which supports
    pointers, and a runtime which actually passes kernel parameters.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>

--- src/microsoft/clc/clc_compiler.c
+++ src/microsoft/clc/clc_compiler.c
@@ -54,12 +54,149 @@ optimize_nir(struct nir_shader *s)
       NIR_PASS(progress, s, nir_opt_algebraic);
       NIR_PASS(progress, s, nir_opt_constant_folding);
       NIR_PASS(progress, s, nir_opt_undef);
+      NIR_PASS(progress, s, nir_opt_deref);
+      NIR_PASS_V(s, nir_lower_explicit_io, nir_var_shader_in | nir_var_mem_global, nir_address_format_32bit_global);
+      NIR_PASS_V(s, nir_lower_system_values);
    } while (progress);
 
    do {
       progress = false;
       NIR_PASS(progress, s, nir_opt_algebraic_late);
    } while (progress);
+}
+
+/*
+ * Transforming CLC into SPIR-V also uses the global memory model for all
+ * access, with a hard requirement on pointers. We don't want that, partly
+ * because it's unclear how to support pointers in DXIL, and partly because
+ * it also requires more work on the runtime side to patch in the pointers
+ * in GPU address space.
+ *
+ * For now, assume that all shader_in global mem accesses will instead be
+ * bound sequentially as SSBOs.
+ */
+static void
+lower_global_mem_to_ssbo(struct nir_shader *nir)
+{
+   /*
+    * First, rewrite all our shader_in inputs (global memory model)
+    * to be SSBOs instead.
+    */
+   foreach_list_typed_safe(nir_variable, in, node, &nir->inputs) {
+      if (in->data.mode != nir_var_shader_in)
+         continue;
+
+      struct nir_variable *ssbo = rzalloc(nir, nir_variable);
+      ssbo->data.mode = nir_var_mem_ssbo;
+      ssbo->data.read_only = in->data.read_only;
+      ssbo->data.location = in->data.location;
+      ssbo->name = in->name ? ralloc_strdup(nir, in->name) : NULL;
+      ssbo->type = in->type;
+
+      nir_shader_add_variable(nir, ssbo);
+      nir->num_shared++;
+      exec_node_remove(&in->node);
+      nir->num_inputs--;
+   }
+
+   foreach_list_typed(nir_function, func, node, &nir->functions) {
+      if (!func->is_entrypoint)
+         continue;
+      assert(func->impl);
+
+      nir_builder b;
+      nir_builder_init(&b, func->impl);
+
+      /*
+       * Now, find all our function params: turn the load deref (pull the
+       * pointer * base address) into an SSBO index which we stash in high
+       * bits.
+       */
+      nir_foreach_block(block, func->impl) {
+         nir_foreach_instr_safe(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+
+            nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
+            if (intr->intrinsic != nir_intrinsic_load_kernel_input)
+               continue;
+            b.cursor = nir_before_instr(instr);
+
+            nir_src *src = &intr->src[0]; /* param location i.e. SSBO slot */
+            assert(src->is_ssa);
+
+            nir_ssa_def *ssbo_loc = nir_ishl(&b, src->ssa, nir_imm_int(&b, 28));
+            nir_ssa_def_rewrite_uses(&intr->dest.ssa,
+                                     nir_src_for_ssa(ssbo_loc));
+            nir_instr_remove(instr);
+         }
+      }
+
+      /*
+       * Find the corresponding store_global instructions and replace them
+       * with SSBO accesses. This requires widening scalar sources to vec4,
+       * and reconstructing the SSBO location + index from the 'pointer'
+       * constructed for us by NIR's IO lowering.
+       */
+      nir_foreach_block(block, func->impl) {
+         nir_foreach_instr_safe(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+
+            nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
+            if (intr->intrinsic != nir_intrinsic_store_global)
+               continue;
+            b.cursor = nir_before_instr(instr);
+
+            assert(intr->src[0].is_ssa);
+            nir_ssa_def *val = intr->src[0].ssa; /* value */
+            nir_ssa_def *vec4_val = nir_vec4(&b, val, val, val, val);
+
+            assert(intr->src[1].is_ssa);
+            nir_ssa_def *ptr = intr->src[1].ssa; /* source 'pointer' */
+#if 0
+            /*
+            * Here's where we would ordinarily do this, and reclaim our SSBO
+            * index from the high bits of the pointer:
+            *  nir_ssa_def *ssbo_loc = nir_ishr(&b, ptr, nir_imm_int(&b, 28));
+            *
+            * However, since we don't implement runtime lookup of SSBO
+            * binding index -> UAV handle yet
+            * (cf. nir_to_dxil.c:emit_store_ssbo), and NIR isn't quite smart
+            * enough to see that: ((x & 0x0fffffff) << 28) >> 28 == 0,
+            * we ... can't actually do this.
+            *
+            * So for just now, you get one SSBO, and all of the above is an
+            * elaborate exercise in misdirection.
+            *
+            * Fixing this would likely involve converting our typed UAVs into
+            * raw, and pushing the handle value into a runtime array; doing
+            * this would bring us closer to the pointer model as we could at
+            * least deal in byte rather than element offsets into the
+            * RWBuffer.
+            */
+            nir_ssa_def *ssbo_loc = nir_ishr(&b, ptr, nir_imm_int(&b, 28));
+#else
+            nir_ssa_def *ssbo_loc = nir_imm_zero(&b, 1, 32);
+#endif
+            nir_ssa_def *ssbo_idx = nir_iand(&b, ptr,
+                                             nir_imm_int(&b, 0x00ffffff));
+            ssbo_idx = nir_ishr(&b, ssbo_idx,
+                                nir_imm_int(&b, val->bit_size / 16));
+
+            nir_intrinsic_instr *store =
+               nir_intrinsic_instr_create(b.shader, nir_intrinsic_store_ssbo);
+            store->num_components = 4;
+            nir_intrinsic_set_write_mask(store, 0xf);
+            nir_intrinsic_set_align(store, 4, 0);
+            store->src[0] = nir_src_for_ssa(vec4_val);
+            store->src[1] = nir_src_for_ssa(ssbo_loc);
+            store->src[2] = nir_src_for_ssa(ssbo_idx);
+            nir_builder_instr_insert(&b, &store->instr);
+            nir_instr_remove(instr);
+         }
+      }
+   }
 }
 
 int clc_compile_from_source(
@@ -87,11 +224,11 @@ int clc_compile_from_source(
       .constant_as_global = true,
       .caps = {
          .address = true,
-	 .float64 = true,
-	 .int8 = true,
-	 .int16 = true,
-	 .int64 = true,
-	 .kernel = true,
+         .float64 = true,
+         .int8 = true,
+         .int16 = true,
+         .int64 = true,
+         .kernel = true,
       },
    };
 
@@ -118,11 +255,31 @@ int clc_compile_from_source(
       return -1;
    }
 
-   nir_print_shader(nir, stderr);
+   NIR_PASS_V(nir, nir_lower_variable_initializers, nir_var_function_temp);
+   NIR_PASS_V(nir, nir_lower_returns);
+   NIR_PASS_V(nir, nir_inline_functions);
+   NIR_PASS_V(nir, nir_opt_deref);
+   foreach_list_typed_safe(nir_function, func, node, &nir->functions) {
+      if (!func->is_entrypoint)
+         exec_node_remove(&func->node);
+   }
+   assert(exec_list_length(&nir->functions) == 1);
+   NIR_PASS_V(nir, nir_lower_variable_initializers, ~nir_var_function_temp);
 
    NIR_PASS_V(nir, nir_lower_system_values);
 
    optimize_nir(nir);
+
+   /*
+    * See comments on these two passes: rewrite global memory-model usage
+    * with pointers into SSBO access. We re-run constant folding and undef
+    * removal as these two passes both generate extra constants.
+    */
+   NIR_PASS_V(nir, lower_global_mem_to_ssbo);
+   NIR_PASS_V(nir, nir_lower_variable_initializers, nir_var_all);
+   optimize_nir(nir);
+
+   nir_print_shader(nir, stderr);
 
    struct blob tmp;
    if (!nir_to_dxil(nir, &tmp)) {
