9d20ee3d0c3baaffa62232990620dbd5f5ee54c4
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-01-29 08:59:28 +0100

    microsoft/compiler: track dxil-features while emitting code

--- src/microsoft/compiler/dxil.h
+++ src/microsoft/compiler/dxil.h
@@ -29,31 +29,6 @@
 #ifndef DXIL_H
 #define DXIL_H
 
-struct dxil_features {
-   unsigned doubles : 1,
-            cs_4x_raw_sb : 1,
-            uavs_at_every_stage : 1,
-            use_64uavs : 1,
-            min_precision : 1,
-            dx11_1_double_extensions : 1,
-            dx11_1_shader_extensions : 1,
-            dx9_comparison_filtering : 1,
-            tiled_resources : 1,
-            stencil_ref : 1,
-            inner_coverage : 1,
-            typed_uav_load_additional_formats : 1,
-            rovs : 1,
-            array_layer_from_vs_or_ds : 1,
-            wave_ops : 1,
-            int64_ops : 1,
-            view_id : 1,
-            barycentrics : 1,
-            native_low_precision : 1,
-            shading_rate : 1,
-            raytracing_tier_1_1 : 1,
-            sampler_feedback : 1;
-};
-
 enum dxil_shader_kind {
    DXIL_PIXEL_SHADER = 0,
    DXIL_VERTEX_SHADER = 1,
