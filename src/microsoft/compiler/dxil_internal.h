4718a99466fd3c43a3ff1de23ed2121a6d07edc7
Gert Wollny <gert.wollny@collabora.com>
2020-02-07 13:43:06 +0100

    microsoft/compiler: Move module structures definition to the header file
    With that we can keep the dumping code separate from the functional code.
    
    v2: - move structs to private header (Erik)
        - keep shader_info because it is not used as pointer
    
    v3: - rebase and add new structures for branch, phi, and extract_value
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

---
+++ src/microsoft/compiler/dxil_internal.h
@@ -0,0 +1,205 @@
+#ifndef DXIL_INTERNAL_H
+#define DXIL_INTERNAL_H
+
+#include "dxil_module.h"
+
+#include "util/list.h"
+
+#include <stdint.h>
+
+struct dxil_type {
+   enum type_type {
+      TYPE_VOID,
+      TYPE_INTEGER,
+      TYPE_FLOAT,
+      TYPE_POINTER,
+      TYPE_STRUCT,
+      TYPE_FUNCTION
+   } type;
+
+   union {
+      unsigned int_bits;
+      unsigned float_bits;
+      const struct dxil_type *ptr_target_type;
+      struct {
+         const char *name;
+         struct dxil_type **elem_types;
+         size_t num_elem_types;
+      } struct_def;
+      struct {
+         const struct dxil_type *ret_type;
+         struct dxil_type **arg_types;
+         size_t num_arg_types;
+      } function_def;
+   };
+
+   struct list_head head;
+   unsigned id;
+};
+
+struct dxil_value {
+   int id;
+};
+
+struct dxil_gvar {
+   const struct dxil_type *type;
+   bool constant;
+   int align;
+
+   struct dxil_value value;
+   struct list_head head;
+};
+
+struct dxil_func {
+   char *name;
+   const struct dxil_type *type;
+   bool decl;
+   unsigned attr_set;
+
+   struct dxil_value value;
+   struct list_head head;
+};
+
+struct dxil_attrib {
+   enum {
+      DXIL_ATTR_ENUM
+   } type;
+
+   union {
+      enum dxil_attr_kind kind;
+   };
+};
+
+struct attrib_set {
+   struct dxil_attrib attrs[2];
+   unsigned num_attrs;
+   struct list_head head;
+};
+
+struct dxil_instr_binop {
+   enum dxil_bin_opcode opcode;
+   const struct dxil_value *operands[2];
+};
+
+struct dxil_instr_cmp {
+   enum dxil_cmp_pred pred;
+   const struct dxil_value *operands[2];
+};
+
+struct dxil_instr_select {
+   const struct dxil_value *operands[3];
+};
+
+struct dxil_instr_cast {
+   enum dxil_cast_opcode opcode;
+   const struct dxil_type *type;
+   const struct dxil_value *value;
+};
+
+struct dxil_instr_call {
+   const struct dxil_func *func;
+   struct dxil_value **args;
+   size_t num_args;
+};
+
+struct dxil_instr_ret {
+   struct dxil_value *value;
+};
+
+struct dxil_instr_extractval {
+   const struct dxil_value *src;
+   const struct dxil_type *type;
+   unsigned int idx;
+};
+
+struct dxil_instr_br {
+   const struct dxil_value *cond;
+   unsigned succ[2];
+};
+
+struct dxil_instr_phi {
+   const struct dxil_type *type;
+   struct {
+      const struct dxil_value *value;
+      unsigned block;
+   } incoming[127];
+   size_t num_incoming;
+};
+
+struct dxil_instr {
+   enum instr_type {
+      INSTR_BINOP,
+      INSTR_CMP,
+      INSTR_SELECT,
+      INSTR_CAST,
+      INSTR_BR,
+      INSTR_PHI,
+      INSTR_CALL,
+      INSTR_RET,
+      INSTR_EXTRACTVAL
+   } type;
+
+   union {
+      struct dxil_instr_binop binop;
+      struct dxil_instr_cmp cmp;
+      struct dxil_instr_select select;
+      struct dxil_instr_cast cast;
+      struct dxil_instr_call call;
+      struct dxil_instr_ret ret;
+      struct dxil_instr_extractval extractval;
+      struct dxil_instr_phi phi;
+      struct dxil_instr_br br;
+   };
+
+   bool has_value;
+   struct dxil_value value;
+
+   struct list_head head;
+};
+
+struct dxil_const {
+   const struct dxil_type *type;
+   struct dxil_value value;
+
+   bool undef;
+   union {
+      intmax_t int_value;
+      double float_value;
+   };
+
+   struct list_head head;
+};
+
+struct dxil_mdnode {
+   enum mdnode_type {
+      MD_STRING,
+      MD_VALUE,
+      MD_NODE
+   } type;
+
+   union {
+      char *string;
+
+      struct {
+         const struct dxil_type *type;
+         const struct dxil_value *value;
+      } value;
+
+      struct {
+         struct dxil_mdnode **subnodes;
+         size_t num_subnodes;
+      } node;
+   };
+
+   struct list_head head;
+   unsigned id;
+};
+
+struct dxil_named_node {
+   char *name;
+   struct dxil_mdnode **subnodes;
+   size_t num_subnodes;
+   struct list_head head;
+};
+
+#endif // DXIL_INTERNAL_H
