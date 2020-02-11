FrostKiwi/mesa

commit 3d9d9dcc4ede424545548e00f1992b6da3b7b837

microsoft/compiler: Add a test backend for the NIR to DXIL translation
The test framework takes a NIR shader as a string, translates it to
a binary NIR shader that is then passed to nir_to_dxil. The resulting
blob is disassembled using the DXCompiler disassembler and the text
is cleaned from comments and the compiler version. The resulting string
is compared against the test expectation.

Also add tests for empty VS, PS, and CS shaders.

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/microsoft/clc/clc_test_dxlayer.cpp
@@ -1,0 +1,208 @@
+/*
+ * Copyright 2020 Collabora Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHOR(S) AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "clc_test_dxlayer.h"
+#include "util/blob.h"
+#include "nir.h"
+#include "microsoft/compiler/nir_to_dxil.h"
+
+#include <d3d12.h>
+#include <dxcapi.h>
+#include <wrl.h>
+
+#include <winerror.h>
+#include <string>
+#include <sstream>
+#include <cassert>
+
+#pragma comment(lib, "dxcompiler")
+
+using std::string;
+using std::stringstream;
+
+static const nir_shader_compiler_options nir_options = { 0 };
+
+using Microsoft::WRL::ComPtr;
+
+class DXILCompiler {
+public:
+   bool disassamble(blob* data, std::string& disassembly);
+   bool validate(blob* data);
+   static DXILCompiler *instance();
+
+private:
+
+
+   DXILCompiler() = default;
+
+   static ComPtr<IDxcLibrary> m_library;
+   static ComPtr<IDxcCompiler> m_compiler;
+   static ComPtr<IDxcValidator> m_validator;
+   static bool m_initialized;
+};
+
+void NirToDXILTest::run(const string& in_shader, const string& dxil_expect) const
+{
+   glsl_type_singleton_init_or_ref();
+
+   nir_shader* shader = nir_shader_from_string(in_shader.c_str(), &nir_options);
+   ASSERT_TRUE(shader != nullptr);
+
+   struct blob tmp;
+   bool convert_success = nir_to_dxil(shader, &tmp);
+   ralloc_free(shader);
+   ASSERT_TRUE(convert_success);
+
+   string dxil_disass;
+   auto compiler =  DXILCompiler::instance();
+   ASSERT_TRUE(compiler != nullptr);
+   bool dissasamble_success = compiler->disassamble(&tmp, dxil_disass);
+   ASSERT_TRUE(dissasamble_success);
+
+   EXPECT_EQ(dxil_disass, dxil_expect);
+
+   bool validate_success = compiler->validate(&tmp);
+   ASSERT_TRUE(validate_success);
+
+   glsl_type_singleton_init_or_ref();
+}
+
+class ShaderBlob : public IDxcBlob {
+public:
+   ShaderBlob(blob* data) : m_data(data) {}
+
+   LPVOID STDMETHODCALLTYPE GetBufferPointer(void) override { return m_data->data; }
+
+   SIZE_T STDMETHODCALLTYPE GetBufferSize() override { return m_data->size; }
+
+   HRESULT STDMETHODCALLTYPE QueryInterface(REFIID, void**) override { return E_NOINTERFACE; }
+
+   ULONG STDMETHODCALLTYPE AddRef() override { return 1; }
+
+   ULONG STDMETHODCALLTYPE Release() override { return 0; }
+
+   blob* m_data;
+};
+
+
+bool DXILCompiler::disassamble(blob *data,
+                               string& disassembly)
+{
+   // Disassamble and convert to std::string
+   ShaderBlob source(data);
+   IDxcBlobEncoding* pDisassembly = nullptr;
+
+   if (FAILED(m_compiler->Disassemble(&source, &pDisassembly)))
+      return false;
+
+   ComPtr<IDxcBlobEncoding> dissassably(pDisassembly);
+   ComPtr<IDxcBlobEncoding> blobUtf8;
+   m_library->GetBlobAsUtf8(pDisassembly, blobUtf8.GetAddressOf());
+   if (!blobUtf8)
+      return false;
+
+   disassembly = reinterpret_cast<const char*>(blobUtf8->GetBufferPointer());
+   disassembly[blobUtf8->GetBufferSize() - 1] = 0;
+
+   stringstream os;
+
+   const char* c = disassembly.c_str();
+   assert(c);
+
+   bool in_comment = false;
+   bool linestart = true;
+   while (*c) {
+
+      if (!in_comment) {
+         if (linestart && ((*c == ';') || (*c == '!' && c[1] == '0'))) {
+               in_comment = true;
+         } else {
+            os << *c;
+         }
+      }
+
+      if (*c == '\n') {
+         linestart = true;
+         in_comment = false;
+      } else
+         linestart = false;
+
+      ++c;
+   }
+   disassembly = os.str();
+   return true;
+}
+
+bool DXILCompiler::validate(blob *data)
+{
+   ShaderBlob source(data);
+   ComPtr<IDxcOperationResult> result;
+   m_validator->Validate(&source, DxcValidatorFlags_InPlaceEdit, &result);
+   HRESULT validationStatus;
+   result->GetStatus(&validationStatus);
+   if (FAILED(validationStatus)) {
+      ComPtr<IDxcBlobEncoding> printBlob, printBlobUtf8;
+      result->GetErrorBuffer(&printBlob);
+      m_library->GetBlobAsUtf8(printBlob.Get(), printBlobUtf8.GetAddressOf());
+
+      std::string errorString;
+      if (printBlobUtf8) {
+         errorString = reinterpret_cast<const char*>(printBlobUtf8->GetBufferPointer());
+      }
+
+      std::cerr << "== VALIDATION ERROR =============================================\n"
+                << errorString  << '\n'
+                << "== END ==========================================================\n";
+
+      return false;
+   }
+   return true;
+}
+
+DXILCompiler *DXILCompiler::instance()
+{
+   // TODO? not reentrant
+   static DXILCompiler me;
+   if (!m_initialized) {
+     HRESULT hr = DxcCreateInstance(CLSID_DxcLibrary, IID_PPV_ARGS(&m_library));
+      if (FAILED(hr))
+         return nullptr;
+
+      hr = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&m_compiler));
+      if (FAILED(hr))
+         return nullptr;
+
+      hr = DxcCreateInstance(CLSID_DxcValidator, IID_PPV_ARGS(&m_validator));
+      if (FAILED(hr))
+         return nullptr;
+
+      m_initialized = true;
+   }
+   return &me;
+}
+
+ComPtr<IDxcLibrary> DXILCompiler::m_library;
+ComPtr<IDxcCompiler> DXILCompiler::m_compiler;
+ComPtr<IDxcValidator> DXILCompiler::m_validator;
+bool DXILCompiler::m_initialized = false;
+

src/microsoft/clc/clc_nir_to_dxil_test.cpp
@@ -1,0 +1,126 @@
+/*
+ * Copyright 2020 Collabora Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHOR(S) AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "nir.h"
+
+#include "clc_test_dxlayer.h"
+
+using std::stringstream;
+
+static const char nir_shader_start[] =
+      R"(shader: MESA_SHADER_)";
+
+static const char nir_shader_end[] =
+      R"(
+inputs: 0
+outputs: 0
+uniforms: 0
+shared: 0
+impl main {
+}
+)";
+
+static const char expect_start[] =
+   R"(target datalayout = "e-m:e-p:32:32-i1:32-i8:32-i16:32-i32:32-i64:64-f16:32-f32:32-f64:64-n8:16:32:64"
+target triple = "dxil-ms-dx"
+
+define void @main() {
+  ret void
+}
+
+!llvm.ident = !{!0}
+!dx.version = !{!1}
+!dx.valver = !{!2}
+!dx.shaderModel = !{!3}
+!dx.typeAnnotations = !{!4}
+!dx.entryPoints = !{!8}
+
+!1 = !{i32 1, i32 0}
+!2 = !{i32 1, i32 4}
+!3 = !{!")";
+
+static const char expect_ps_end[] =
+R"(", i32 6, i32 0}
+!4 = !{i32 1, void ()* @main, !5}
+!5 = !{!6}
+!6 = !{i32 0, !7, !7}
+!7 = !{}
+!8 = !{void ()* @main, !"main", !9, null, null}
+!9 = !{null, !7, null}
+)";
+
+static const char expect_end[] =
+R"(", i32 6, i32 0}
+!4 = !{i32 1, void ()* @main, !5}
+!5 = !{!6}
+!6 = !{i32 0, !7, !7}
+!7 = !{}
+!8 = !{void ()* @main, !"main", null, null, null}
+)";
+
+const  char expect_cs_end[] =
+R"(", i32 6, i32 0}
+!4 = !{i32 1, void ()* @main, !5}
+!5 = !{!6}
+!6 = !{i32 0, !7, !7}
+!7 = !{}
+!8 = !{void ()* @main, !"main", null, null, !9}
+!9 = !{i32 4, !10}
+!10 = !{i32 1, i32 1, i32 1}
+)";
+
+TEST_F(NirToDXILTest, test_empty_VS_shader)
+{
+   stringstream shader;
+   shader << nir_shader_start << "VERTEX" << nir_shader_end;
+
+   stringstream expect;
+   expect << expect_start << "vs" << expect_end;
+
+   run(shader.str(), expect.str());
+}
+
+TEST_F(NirToDXILTest, test_empty_PS_shader)
+{
+   stringstream shader;
+   shader << nir_shader_start << "FRAGMENT" << nir_shader_end;
+
+   stringstream expect;
+   expect << expect_start << "ps" << expect_end;
+
+   run(shader.str(), expect.str());
+}
+
+TEST_F(NirToDXILTest, test_empty_CS_shader)
+{
+   stringstream shader;
+   shader << nir_shader_start << "COMPUTE" << nir_shader_end;
+
+   stringstream expect;
+   expect << expect_start << "cs" << expect_cs_end;
+
+   run(shader.str(), expect.str());
+}
+
+
+

src/microsoft/clc/clc_test_dxlayer.h
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2020 Collabora Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * on the rights to use, copy, modify, merge, publish, distribute, sub
+ * license, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHOR(S) AND/OR THEIR SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#ifndef DXLAYER_H
+#define DXLAYER_H
+
+#include "util/blob.h"
+#include "gtest/gtest.h"
+
+#include <string>
+
+class NirToDXILTest : public ::testing::Test {
+protected:
+   void run(const std::string& nir_shader, const std::string& dxil_expect) const;
+};
+
+#endif // DXLAYER_H

src/microsoft/clc/meson.build
@@ -40,4 +40,14 @@ clc_compiler_test = executable('clc_compiler_test', 'clc_compiler_test.cpp',
   link_with : [libclc_compiler],
   dependencies : [idep_gtest])
 
-test('clc_compiler test', clc_compiler_test)
+test('clc_nir_to_dxil',
+  executable(
+   'clc_nir_to_dxil_test',
+    ['clc_nir_to_dxil_test.cpp', 'clc_test_dxlayer.cpp'],
+    cpp_args : [cpp_vis_args, cpp_msvc_compat_args],
+    include_directories : [inc_common],
+    dependencies : [idep_libdxil_compiler, dep_thread, idep_gtest, idep_nir, idep_mesautil],
+    link_with : [libclc_compiler]
+  )
+)
+

commit bcf710c081d28455840a3171463c34e3b4b78594

microsift/tests: Force a newline at the end of the dxil disassembler output
It seems that different versions of the compiler/disassembler behave
differently here.

Fixes: 3d9d9dcc4ede424545548e00f1992b6da3b7b837
    microsoft/compiler: Add a test backend for the NIR to DXIL translation

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/microsoft/clc/clc_test_dxlayer.cpp
@@ -149,6 +149,11 @@ bool DXILCompiler::disassamble(blob *data,
 
       ++c;
    }
+   /* Different versions of dxcompiler seem to disagree whether a new line should
+    * be added at the end or not */
+   if (!linestart)
+      os << '\n';
+
    disassembly = os.str();
    return true;
 }