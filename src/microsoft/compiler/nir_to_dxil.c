71ce1d96101e2bf5ca36e24d39b77698977a4b74
Gert Wollny <gert.wollny@collabora.com>
2020-03-02 08:55:38 +0100

    microsoft/compiler: Handle nir_op_fsin
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

--- src/microsoft/compiler/nir_to_dxil.c
+++ src/microsoft/compiler/nir_to_dxil.c
@@ -156,6 +156,7 @@ enum dxil_intr {
    DXIL_INTR_FABS = 6,
 
    DXIL_INTR_FCOS = 12,
+   DXIL_INTR_FSIN = 13,
    DXIL_INTR_FRC = 22,
 
    DXIL_INTR_ROUND_NE = 26,
@@ -1132,6 +1133,7 @@ emit_alu(struct ntd_context *ctx, nir_alu_instr *alu)
    case nir_op_ftrunc: return emit_unary_intin(ctx, alu, DXIL_INTR_ROUND_Z, src[0]);
    case nir_op_fabs: return emit_unary_intin(ctx, alu, DXIL_INTR_FABS, src[0]);
    case nir_op_fcos: return emit_unary_intin(ctx, alu, DXIL_INTR_FCOS, src[0]);
+   case nir_op_fsin: return emit_unary_intin(ctx, alu, DXIL_INTR_FSIN, src[0]);
    case nir_op_fceil: return emit_unary_intin(ctx, alu, DXIL_INTR_ROUND_PI, src[0]);
    case nir_op_ffloor: return emit_unary_intin(ctx, alu, DXIL_INTR_ROUND_NI, src[0]);
    case nir_op_ffract: return emit_unary_intin(ctx, alu, DXIL_INTR_FRC, src[0]);
