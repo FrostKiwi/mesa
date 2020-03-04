4a3f45e65611bcc9d0f9c0c9ad930d59b1c6c082
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-03-04 11:57:16 +0100

    d3d12: do not pass incorrect usage-bits to buffer-creation
    When I cleaned up this function, I messed this up. Sorry for the mess!
    
    Fixes: 022118261bf ("d3d12: Textures are uploaded through a staging resource")

--- src/gallium/drivers/d3d12/d3d12_resource.cpp
+++ src/gallium/drivers/d3d12/d3d12_resource.cpp
@@ -300,7 +300,7 @@ d3d12_transfer_map(struct pipe_context *pctx,
                                                      box->height);
 
       trans->staging_res = pipe_buffer_create(pctx->screen, 0,
-                                              (pipe_resource_usage)(PIPE_USAGE_STAGING | usage),
+                                              PIPE_USAGE_STAGING,
                                               ptrans->layer_stride * box->depth);
       if (!trans->staging_res)
          return NULL;
