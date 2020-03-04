4b44131dbf86acdac83dffdba45627cd3216072d
Gert Wollny <gert.wollny@collabora.com>
2020-03-04 08:24:39 +0100

    microsoft/compiler: Handle nir_op_feq
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

--- src/microsoft/compiler/nir_to_dxil.c
+++ src/microsoft/compiler/nir_to_dxil.c
@@ -1345,6 +1345,7 @@ emit_alu(struct ntd_context *ctx, nir_alu_instr *alu)
    case nir_op_uge:  return emit_cmp(ctx, alu, DXIL_ICMP_UGE, src[0], src[1]);
    case nir_op_ilt:  return emit_cmp(ctx, alu, DXIL_ICMP_SLT, src[0], src[1]);
    case nir_op_ult:  return emit_cmp(ctx, alu, DXIL_ICMP_ULT, src[0], src[1]);
+   case nir_op_feq:  return emit_cmp(ctx, alu, DXIL_FCMP_UEQ, src[0], src[1]);
    case nir_op_flt:  return emit_cmp(ctx, alu, DXIL_FCMP_ULT, src[0], src[1]);
    case nir_op_fge:  return emit_cmp(ctx, alu, DXIL_FCMP_UGE, src[0], src[1]);
    case nir_op_bcsel: return emit_select(ctx, alu, src[0], src[1], src[2]);
