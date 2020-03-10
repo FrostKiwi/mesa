7454e62958bd774dabeca9b6e1b82acb61d71678
Louis-Francis Ratté-Boulianne <lfrb@collabora.com>
2020-03-10 14:07:41 -0400

    gallium/nir: Wrap tgsi_to_nir header in extern C

--- src/gallium/auxiliary/nir/tgsi_to_nir.h
+++ src/gallium/auxiliary/nir/tgsi_to_nir.h
@@ -27,6 +27,10 @@
 #include "compiler/nir/nir.h"
 #include "pipe/p_screen.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct nir_shader *
 tgsi_to_nir(const void *tgsi_tokens,
             struct pipe_screen *screen);
@@ -34,5 +38,9 @@ tgsi_to_nir(const void *tgsi_tokens,
 struct nir_shader *
 tgsi_to_nir_noscreen(const void *tgsi_tokens,
                      const nir_shader_compiler_options *options);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
