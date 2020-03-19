ab2a19f308489bb5b53c40481385096ece13eefd
Louis-Francis Ratté-Boulianne <lfrb@collabora.com>
2020-03-19 16:11:35 -0400

    microsoft/compiler: Add support for emit_vertex/end_primitive

--- src/microsoft/compiler/dxil_function.c
+++ src/microsoft/compiler/dxil_function.c
@@ -56,6 +56,8 @@ static struct  predefined_func_descr predefined_funcs[] = {
 {"dx.op.sampleCmp", "R", "i@@ffffiiiff", DXIL_ATTR_KIND_READ_ONLY},
 {"dx.op.textureLoad", "R", "i@iiiiiii", DXIL_ATTR_KIND_READ_ONLY},
 {"dx.op.discard", "v", "ib", DXIL_ATTR_KIND_READ_NONE},
+{"dx.op.emitStream", "v", "ic", DXIL_ATTR_KIND_NONE},
+{"dx.op.cutStream", "v", "ic", DXIL_ATTR_KIND_NONE},
 };
 
 struct func_descr {
