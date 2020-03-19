FrostKiwi/mesa

commit 5b0295ebb5a8462425f28d1bff40c6728daa35a1

d3d12: Add support for constant buffers
v2: - Use the PIPE_SHADER_STAGES as index limit and for the
      allocation size (Erik)
    - Use pipe shader stage
    - Use unsigned loop counter to fix comparison signed/unsigned
v3: - Use D3D12_GFX_SHADER_STAGES as loop limit
    - pass shader stage instead of making it a member

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/gallium/drivers/d3d12/d3d12_compiler.cpp
@@ -30,6 +30,7 @@
 
 #include "nir.h"
 #include "compiler/nir/nir_builder.h"
+#include "tgsi/tgsi_from_mesa.h"
 
 #include "util/u_memory.h"
 
@@ -83,6 +84,12 @@ d3d12_compile_nir(struct d3d12_context *ctx, struct nir_shader *nir)
    if (!nir_to_dxil(nir, &tmp)) {
       debug_printf("D3D12: nir_to_dxil failed\n");
       return NULL;
+   }
+
+   enum pipe_shader_type stage = pipe_shader_type_from_mesa(nir->info.stage);
+   nir_foreach_variable(var, &nir->uniforms) {
+      if (var->interface_type)
+         ret->cb_bindings[ret->num_cb_bindings++] = var->data.binding;
    }
 
    ctx->validation_tools->validate_and_sign(&tmp);

src/gallium/drivers/d3d12/d3d12_compiler.h
@@ -33,6 +33,11 @@
 
 struct pipe_screen;
 
+typedef enum {
+   D3D12_BINDING_CONSTANT_BUFFER,
+   D3D12_NUM_BINDING_TYPES
+} D3D12_BINDING_TYPE;
+
 struct d3d12_validation_tools *d3d12_validator_create();
 
 void d3d12_validator_destroy(struct d3d12_validation_tools *validator);
@@ -47,6 +52,9 @@ struct d3d12_shader {
    size_t bytecode_length;
 
    shader_info info;
+
+   unsigned cb_bindings[PIPE_MAX_CONSTANT_BUFFERS];
+   size_t num_cb_bindings;
 };
 
 struct d3d12_shader *

src/gallium/drivers/d3d12/d3d12_context.cpp
@@ -31,6 +31,7 @@
 
 #include "util/u_framebuffer.h"
 #include "util/u_helpers.h"
+#include "util/u_inlines.h"
 #include "util/u_memory.h"
 #include "util/u_upload_mgr.h"
 #include "microsoft/compiler/nir_to_dxil.h"
@@ -54,6 +55,7 @@ d3d12_context_destroy(struct pipe_context *pctx)
    CloseHandle(ctx->event);
    d3d12_descriptor_heap_free(ctx->rtv_heap);
    d3d12_descriptor_heap_free(ctx->dsv_heap);
+   d3d12_descriptor_heap_free(ctx->view_heap);
    util_primconvert_destroy(ctx->primconvert);
    slab_destroy_child(&ctx->transfer_pool);
    FREE(ctx);
@@ -596,6 +598,30 @@ d3d12_set_constant_buffer(struct pipe_context *pctx,
                           enum pipe_shader_type shader, uint index,
                           const struct pipe_constant_buffer *buf)
 {
+   struct d3d12_context *ctx = d3d12_context(pctx);
+
+   if (buf) {
+      struct pipe_resource *buffer = buf->buffer;
+      unsigned offset = buf->buffer_offset;
+      if (buf->user_buffer) {
+         u_upload_data(pctx->const_uploader, 0, buf->buffer_size,
+                       D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT,
+                       buf->user_buffer, &offset, &buffer);
+      }
+
+      pipe_resource_reference(&ctx->cbufs[shader][index].buffer, buffer);
+      ctx->cbufs[shader][index].buffer_offset = offset;
+      ctx->cbufs[shader][index].buffer_size = buf->buffer_size;
+      ctx->cbufs[shader][index].user_buffer = NULL;
+
+      if (buf->user_buffer)
+         pipe_resource_reference(&buffer, NULL);
+   } else {
+      pipe_resource_reference(&ctx->cbufs[shader][index].buffer, NULL);
+      ctx->cbufs[shader][index].buffer_offset = 0;
+      ctx->cbufs[shader][index].buffer_size = 0;
+      ctx->cbufs[shader][index].user_buffer = NULL;
+   }
 }
 
 static void
@@ -833,6 +859,15 @@ d3d12_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
                                              D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
                                              10);
    if (!ctx->dsv_heap) {
+      FREE(ctx);
+      return NULL;
+   }
+
+   ctx->view_heap = d3d12_descriptor_heap_new(screen->dev,
+                                              D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+                                              D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+                                              PIPE_SHADER_TYPES * PIPE_MAX_CONSTANT_BUFFERS);
+   if (!ctx->view_heap) {
       FREE(ctx);
       return NULL;
    }

src/gallium/drivers/d3d12/d3d12_context.h
@@ -61,6 +61,7 @@ struct d3d12_context {
    struct slab_child_pool transfer_pool;
    struct primconvert_context *primconvert;
 
+   struct pipe_constant_buffer cbufs[PIPE_SHADER_TYPES][PIPE_MAX_CONSTANT_BUFFERS];
    struct pipe_framebuffer_state fb;
    struct d3d12_vertex_elements_state *ves;
    struct pipe_vertex_buffer vbs[PIPE_MAX_ATTRIBS];
@@ -88,6 +89,7 @@ struct d3d12_context {
 
    struct d3d12_descriptor_heap *rtv_heap;
    struct d3d12_descriptor_heap *dsv_heap;
+   struct d3d12_descriptor_heap *view_heap;
 
    PFN_D3D12_SERIALIZE_VERSIONED_ROOT_SIGNATURE D3D12SerializeVersionedRootSignature;
    struct d3d12_validation_tools *validation_tools;

src/gallium/drivers/d3d12/d3d12_draw.cpp
@@ -31,19 +31,82 @@
 #include "util/u_debug.h"
 #include "util/u_helpers.h"
 #include "util/u_prim.h"
+#include "util/u_math.h"
 
 extern "C" {
 #include "indices/u_primconvert.h"
+}
+
+static D3D12_SHADER_VISIBILITY
+get_shader_visibility(enum pipe_shader_type stage)
+{
+   switch (stage) {
+   case PIPE_SHADER_VERTEX:
+      return D3D12_SHADER_VISIBILITY_VERTEX;
+   case PIPE_SHADER_FRAGMENT:
+      return D3D12_SHADER_VISIBILITY_PIXEL;
+   case PIPE_SHADER_GEOMETRY:
+      return D3D12_SHADER_VISIBILITY_GEOMETRY;
+   case PIPE_SHADER_TESS_CTRL:
+      return D3D12_SHADER_VISIBILITY_HULL;
+   case PIPE_SHADER_TESS_EVAL:
+      return D3D12_SHADER_VISIBILITY_DOMAIN;
+   default:
+      unreachable("unknown shader stage");
+   }
+}
+
+static inline void
+init_root_parameter(D3D12_ROOT_PARAMETER1 *param,
+                    D3D12_DESCRIPTOR_RANGE1 *range,
+                    D3D12_DESCRIPTOR_RANGE_TYPE type,
+                    uint32_t num_descs,
+                    D3D12_SHADER_VISIBILITY visibility)
+{
+   range->RangeType = type;
+   range->NumDescriptors = num_descs;
+   range->BaseShaderRegister = 0; /* We only have one range/table for each desc type */
+   range->RegisterSpace = 0;
+   range->Flags = D3D12_DESCRIPTOR_RANGE_FLAG_DATA_VOLATILE;
+   range->OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+   param->ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+   param->DescriptorTable.NumDescriptorRanges = 1;
+   param->DescriptorTable.pDescriptorRanges = range;
+   param->ShaderVisibility = visibility;
 }
 
 static ID3D12RootSignature *
 get_root_signature(struct d3d12_context *ctx)
 {
    struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+   D3D12_ROOT_PARAMETER1 root_params[PIPE_SHADER_TYPES * D3D12_NUM_BINDING_TYPES];
+   D3D12_DESCRIPTOR_RANGE1 desc_ranges[PIPE_SHADER_TYPES * D3D12_NUM_BINDING_TYPES];
+   unsigned num_params = 0;
+
+   for (unsigned i = 0; i < D3D12_GFX_SHADER_STAGES; ++i) {
+      struct d3d12_shader *shader = ctx->gfx_stages[i];
+
+      if (!shader)
+         continue;
+
+      D3D12_SHADER_VISIBILITY visibility = get_shader_visibility((enum pipe_shader_type)i);
+
+      if (shader->num_cb_bindings > 0) {
+         assert(num_params < PIPE_SHADER_TYPES * D3D12_NUM_BINDING_TYPES);
+         init_root_parameter(&root_params[num_params],
+                             &desc_ranges[num_params],
+                             D3D12_DESCRIPTOR_RANGE_TYPE_CBV,
+                             shader->num_cb_bindings,
+                             visibility);
+         num_params++;
+      }
+   }
+
    D3D12_VERSIONED_ROOT_SIGNATURE_DESC root_sig_desc;
    root_sig_desc.Version = D3D_ROOT_SIGNATURE_VERSION_1_1;
-   root_sig_desc.Desc_1_1.NumParameters = 0;
-   root_sig_desc.Desc_1_1.pParameters = NULL;
+   root_sig_desc.Desc_1_1.NumParameters = num_params;
+   root_sig_desc.Desc_1_1.pParameters = (num_params > 0) ? root_params : NULL;
    root_sig_desc.Desc_1_1.NumStaticSamplers = 0;
    root_sig_desc.Desc_1_1.pStaticSamplers = NULL;
    root_sig_desc.Desc_1_1.Flags = D3D12_ROOT_SIGNATURE_FLAG_NONE;
@@ -68,6 +131,59 @@ get_root_signature(struct d3d12_context *ctx)
       return NULL;
    }
    return ret;
+}
+
+static D3D12_GPU_DESCRIPTOR_HANDLE
+fill_cbv_descriptors(struct d3d12_context *ctx,
+                     struct d3d12_shader *shader,
+                     int stage)
+{
+   struct d3d12_descriptor_handle table_start;
+   d2d12_descriptor_heap_get_next_handle(ctx->view_heap, &table_start);
+
+   for (unsigned i = 0; i < shader->num_cb_bindings; i++) {
+      unsigned index = shader->cb_bindings[i];
+      struct pipe_constant_buffer *buffer = &ctx->cbufs[stage][index];
+
+      assert(buffer->buffer_size > 0);
+      assert(buffer->buffer);
+
+      struct d3d12_resource *res = d3d12_resource(buffer->buffer);
+      D3D12_CONSTANT_BUFFER_VIEW_DESC cbv_desc = {};
+      cbv_desc.BufferLocation = res->res->GetGPUVirtualAddress() + buffer->buffer_offset;
+
+      cbv_desc.SizeInBytes = align(buffer->buffer_size , 256);
+
+      struct d3d12_descriptor_handle handle;
+      d3d12_descriptor_heap_alloc_handle(ctx->view_heap, &handle);
+      d3d12_screen(ctx->base.screen)->dev->CreateConstantBufferView(&cbv_desc, handle.cpu_handle);
+   }
+
+   return table_start.gpu_handle;
+}
+
+static unsigned
+fill_descriptor_tables(struct d3d12_context *ctx,
+                       ID3D12DescriptorHeap **heaps,
+                       D3D12_GPU_DESCRIPTOR_HANDLE *tables)
+{
+   unsigned num_tables = 0;
+
+   d3d12_descriptor_heap_clear(ctx->view_heap);
+
+   for (unsigned i = 0; i < D3D12_GFX_SHADER_STAGES; ++i) {
+      struct d3d12_shader *shader = ctx->gfx_stages[i];
+
+      if (!shader)
+         continue;
+
+      if (shader->num_cb_bindings > 0) {
+         heaps[num_tables] = d3d12_descriptor_heap_get(ctx->view_heap);
+         tables[num_tables++] = fill_cbv_descriptors(ctx, shader, i);
+      }
+   }
+
+   return num_tables;
 }
 
 static D3D12_PRIMITIVE_TOPOLOGY_TYPE
@@ -217,10 +333,6 @@ d3d12_draw_vbo(struct pipe_context *pctx,
       return;
    }
 
-   ID3D12RootSignature *root_sig = get_root_signature(ctx);
-   ID3D12PipelineState *pipeline_state = get_gfx_pipeline_state(ctx, root_sig,
-                                                                u_reduced_prim(dinfo->mode));
-
    unsigned index_offset = 0;
    struct pipe_resource *index_buffer = NULL;
    if (dinfo->index_size > 0) {
@@ -234,7 +346,18 @@ d3d12_draw_vbo(struct pipe_context *pctx,
          index_buffer = dinfo->index.resource;
    }
 
+   ID3D12RootSignature *root_sig = get_root_signature(ctx);
+   ID3D12PipelineState *pipeline_state = get_gfx_pipeline_state(ctx, root_sig,
+                                                                u_reduced_prim(dinfo->mode));
    ctx->cmdlist->SetGraphicsRootSignature(root_sig);
+
+   ID3D12DescriptorHeap *heaps[PIPE_SHADER_TYPES * D3D12_NUM_BINDING_TYPES];
+   D3D12_GPU_DESCRIPTOR_HANDLE tables[PIPE_SHADER_TYPES * D3D12_NUM_BINDING_TYPES];
+   unsigned num_tables = fill_descriptor_tables(ctx, heaps, tables);
+   ctx->cmdlist->SetDescriptorHeaps(num_tables, heaps);
+   for (unsigned i = 0; i < num_tables; ++i) {
+      ctx->cmdlist->SetGraphicsRootDescriptorTable(i, tables[i]);
+   }
 
    ctx->cmdlist->RSSetViewports(ctx->num_viewports, ctx->viewports);
    if (ctx->num_scissors > 0)

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -247,6 +247,9 @@ d3d12_get_param(struct pipe_screen *pscreen, enum pipe_cap param)
       return screen->max_feature_level >= D3D_FEATURE_LEVEL_10_0;
 #endif
 
+   case PIPE_CAP_CONSTANT_BUFFER_OFFSET_ALIGNMENT:
+      return 256;
+
    case PIPE_CAP_PCI_GROUP:
    case PIPE_CAP_PCI_BUS:
    case PIPE_CAP_PCI_DEVICE:

commit ea6b0a99793e4837d1f78e691aad2378094433ba

d3d12: remove unused stage variable
Fixes: 5b0295ebb5a ("d3d12: Add support for constant buffers")

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/gallium/drivers/d3d12/d3d12_compiler.cpp
@@ -134,7 +134,6 @@ d3d12_compile_nir(struct d3d12_context *ctx, struct nir_shader *nir)
       return NULL;
    }
 
-   enum pipe_shader_type stage = pipe_shader_type_from_mesa(nir->info.stage);
    nir_foreach_variable(var, &nir->uniforms) {
       if (glsl_type_is_sampler(var->type)) {
          shader->srv_bindings[shader->num_srv_bindings].index = var->data.binding;