FrostKiwi/mesa

commit 053250d06bd5e2a1858a1ee19c16179979b1918f

microsoft/compiler: don't dump condition in unconditional branches
Fixes: aca7c52e3a7a9bceaeee26abf86b223ed7ede9a9
    microsoft/compiler: Add dumping of branches

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/microsoft/compiler/dxil_dump.c
@@ -471,7 +471,10 @@ static void
 dump_instr_branch(struct dxil_dumper *d, struct dxil_instr_br *br)
 {
    _mesa_string_buffer_append(d->buf, "branch ");
-   dump_value(d->buf, br->cond);
+   if (br->cond)
+      dump_value(d, br->cond);
+   else
+      _mesa_string_buffer_append(d->buf, " (uncond)");
    _mesa_string_buffer_printf(d->buf, " %d %d", br->succ[0], br->succ[1]);
 }
 

commit 39017b576c8926f514d3148afe6ad1b9d0c2e4ae

microsoft/compiler: fix argument to dump_value
Fixes: 053250d06bd ("microsoft/compiler: don't dump condition in unconditional branches")

src/microsoft/compiler/dxil_dump.c
@@ -472,7 +472,7 @@ dump_instr_branch(struct dxil_dumper *d, struct dxil_instr_br *br)
 {
    _mesa_string_buffer_append(d->buf, "branch ");
    if (br->cond)
-      dump_value(d, br->cond);
+      dump_value(d->buf, br->cond);
    else
       _mesa_string_buffer_append(d->buf, " (uncond)");
    _mesa_string_buffer_printf(d->buf, " %d %d", br->succ[0], br->succ[1]);