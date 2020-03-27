9f9705e4374018a50e41954f868b0c80cc44567b
Boris Brezillon <boris.brezillon@collabora.com>
2020-03-27 11:34:23 +0100

    microsoft/clc: Support linking several clc objects
    Linking happens at the SPIR-V level, and we rely on SPIRV-Tools to do
    that.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- src/microsoft/clc/clc_helpers.h
+++ src/microsoft/clc/clc_helpers.h
@@ -38,6 +38,12 @@ clc_to_spirv(const struct clc_compile_args *args,
              struct spirv_binary *spvbin,
              char **err_buf);
 
+int
+clc_link_spirv_binaries(const struct spirv_binary **in_bins,
+                        unsigned num_in_bins,
+                        struct spirv_binary *dst_bin,
+                        char **err_buf);
+
 void
 clc_free_spirv_binary(struct spirv_binary *spvbin);
 
