75ea2ffeb81cc016c5e97884d48a5a8a6ec63c91
Gert Wollny <gert.wollny@collabora.com>
2020-04-07 15:11:34 +0200

    d3d12: lower state variables after lowering to UBOs
    Since we want the state variables to be put last we should create the
    default UBO first to avoid needless shifting of the bindings.
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

--- src/gallium/drivers/d3d12/d3d12_compiler.cpp
+++ src/gallium/drivers/d3d12/d3d12_compiler.cpp
@@ -129,9 +129,8 @@ compile_nir(struct d3d12_context *ctx, struct d3d12_shader_selector *sel,
    NIR_PASS_V(nir, nir_lower_tex, &tex_options);
    NIR_PASS_V(nir, nir_lower_clip_halfz);
    NIR_PASS_V(nir, d3d12_lower_yflip);
-
-   NIR_PASS_V(nir, d3d12_lower_state_vars, shader);
    NIR_PASS_V(nir, nir_lower_uniforms_to_ubo, 16);
+   NIR_PASS_V(nir, d3d12_lower_state_vars, shader);
    NIR_PASS_V(nir, d3d12_lower_bool_loads);
 
    struct nir_to_dxil_options opts = {};
