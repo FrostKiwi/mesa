FrostKiwi/mesa

commit 8d3b1c75579bf0f95a32e8832707b714171a0606

microsoft/compiler: Drop the ctx->num_cbvs == 1 asserts
We are about to create an extra CBV for kernel inputs. Let's drop those
asserts and replace them by others when that makes sense.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

src/microsoft/compiler/nir_to_dxil.c
@@ -720,6 +720,7 @@ emit_cbv(struct ntd_context *ctx, nir_variable *var)
    unsigned binding = var->data.binding;
    unsigned size = get_dword_size(var->type);
 
+   assert(idx == binding);
    assert(idx < ARRAY_SIZE(ctx->cbv_metadata_nodes));
    assert(binding < ARRAY_SIZE(ctx->cbv_handles));
 
@@ -742,6 +743,7 @@ emit_cbv(struct ntd_context *ctx, nir_variable *var)
    if (!handle)
       return false;
 
+   assert(!ctx->cbv_handles[binding]);
    ctx->cbv_handles[binding] = handle;
    ctx->num_cbvs++;
 
@@ -1819,7 +1821,7 @@ emit_load_ubo(struct ntd_context *ctx, nir_intrinsic_instr *intr)
    assert(const_block_index); // no dynamic indexing for now
    assert(const_block_index->u32 <= 1); // we only support the default and state vars UBO for now
    const struct dxil_value *handle = ctx->cbv_handles[const_block_index->u32];
-
+   assert(handle);
    const struct dxil_value *offset;
    nir_const_value *const_offset = nir_src_as_const_value(intr->src[1]);
    if (const_offset) {
@@ -2002,9 +2004,8 @@ emit_load_mem_ubo(struct ntd_context *ctx, nir_intrinsic_instr *intr,
       bit_size);
    unsigned align = bit_size / 8;
 
-   // HACK: force CBV#0
-   assert(ctx->num_cbvs == 1);
-   const struct dxil_value *handle = ctx->cbv_handles[0]; // HACK!
+   const struct dxil_value *handle = ctx->cbv_handles[var->data.binding];
+   assert(handle);
    const struct dxil_value *index = offset_to_index(&ctx->mod, ptr, bit_size * 4);
    const struct dxil_value *agg = load_ubo(ctx, handle, index, DXIL_I32);
    if (!index || !agg)
@@ -2231,7 +2232,6 @@ emit_deref_array(struct ntd_context *ctx, nir_deref_instr *deref)
 
    case nir_var_mem_ubo: {
          // HACK: force CBV#0
-         assert(ctx->num_cbvs == 1);
          unsigned bit_size = glsl_get_bit_size(glsl_without_array(var->type));
          ptr = index_to_offset(&ctx->mod, index, bit_size);
          store_dest_int(ctx, &deref->dest, 0, ptr);

commit c3666873fefd66f246381c4f99eeed3295cd2fa5

microsoft/compiler: Drop assert, it makes wrong assumtions
This assert assumes that the constant buffers are ordered in ascending
binding order, but this is not really required, and there is no need to
make the resource ID equal to the binding.

Fixes:  8d3b1c75579bf0f95a32e8832707b714171a0606
    microsoft/compiler: Drop the ctx->num_cbvs == 1 asserts

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/microsoft/compiler/nir_to_dxil.c
@@ -759,7 +759,6 @@ emit_cbv(struct ntd_context *ctx, nir_variable *var)
    unsigned binding = var->data.binding;
    unsigned size = get_dword_size(var->type);
 
-   assert(idx == binding);
    assert(idx < ARRAY_SIZE(ctx->cbv_metadata_nodes));
    assert(binding < ARRAY_SIZE(ctx->cbv_handles));
 