FrostKiwi/mesa

commit 9c177966cafdc109a717eca7b01024fe72fb6c6e

fixup! nir: Always create UBO variable when lowering uniforms to ubo

src/compiler/nir/nir_lower_uniforms_to_ubo.c
@@ -102,21 +102,22 @@ nir_lower_uniforms_to_ubo(nir_shader *shader, int multiplier)
    }
 
    if (progress) {
-      assert(shader->num_uniforms > 0);
-      const struct glsl_type *type = glsl_array_type(glsl_vec4_type(),
-                                                     shader->num_uniforms, 0);
-      nir_variable *ubo = nir_variable_create(shader, nir_var_mem_ubo, type,
-                                              "uniform_0");
-      ubo->data.binding = 0;
+      if (shader->num_uniforms > 0) {
+         const struct glsl_type *type = glsl_array_type(glsl_vec4_type(),
+                                                        shader->num_uniforms, 0);
+         nir_variable *ubo = nir_variable_create(shader, nir_var_mem_ubo, type,
+                                                 "uniform_0");
+         ubo->data.binding = 0;
 
-      struct glsl_struct_field field = {
-         .type = type,
-         .name = "data",
-         .location = -1,
-      };
-      ubo->interface_type =
-            glsl_interface_type(&field, 1, GLSL_INTERFACE_PACKING_STD430,
-                                false, "__ubo0_interface");
+         struct glsl_struct_field field = {
+            .type = type,
+            .name = "data",
+            .location = -1,
+         };
+         ubo->interface_type =
+               glsl_interface_type(&field, 1, GLSL_INTERFACE_PACKING_STD430,
+                                   false, "__ubo0_interface");
+      }
    }
 
    shader->info.first_ubo_is_default_ubo = true;

commit e5aa28c6db3f231be0cab03178c46ad462f93cbf

nir: lower_uniforms_to_ubo: Increment num_ubos when needed
Fixes: 9c177966cafdc109a717eca7b01024fe72fb6c6e
    fixup! nir: Always create UBO variable when lowering uniforms to ubo

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/compiler/nir/nir_lower_uniforms_to_ubo.c
@@ -122,6 +122,7 @@ nir_lower_uniforms_to_ubo(nir_shader *shader, int multiplier)
          ubo->interface_type =
                glsl_interface_type(&field, 1, GLSL_INTERFACE_PACKING_STD430,
                                    false, "__ubo0_interface");
+         shader->info.num_ubos++;
       }
    }
 