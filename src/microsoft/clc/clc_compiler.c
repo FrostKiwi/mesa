FrostKiwi/mesa

commit 802eed03579a4c82fe5a2117215c8af82878992c

microsoft/compiler: lower global load/store to dxil intrinsics
This allows us to do this in a bit more general code, and allows us to
optimize some of this code as well.

src/microsoft/clc/clc_compiler.c
@@ -28,6 +28,136 @@
 #include "util/u_debug.h"
 #include <util/u_math.h>
 #include "spirv/nir_spirv.h"
+#include "nir_builder.h"
+
+/*
+ * DXIL doesn't support reading and writing global memory through pointers,
+ * but needs a buffer-index and offset instead. This code lowers this to
+ * DXIL specific intrinsics, so we can deal with these limitations as early
+ * as possible.
+ *
+ * In principle, this is very similar to SSBOs, so at some point we might
+ * want to unify that.
+ */
+
+static void
+lower_load_kernel_input(nir_builder *b, nir_intrinsic_instr *intr)
+{
+   b->cursor = nir_before_instr(&intr->instr);
+
+   nir_src *src = &intr->src[0]; /* param location i.e. SSBO slot */
+   assert(src->is_ssa);
+
+   nir_ssa_def *ssbo_loc = nir_ishl(b, src->ssa, nir_imm_int(b, 28 - 2));
+   nir_ssa_def_rewrite_uses(&intr->dest.ssa,
+                            nir_src_for_ssa(ssbo_loc));
+   nir_instr_remove(&intr->instr);
+}
+
+static nir_ssa_def *
+ptr_to_buffer(nir_builder *b, nir_ssa_def *ptr)
+{
+   return nir_ishr(b, ptr, nir_imm_int(b, 28));
+}
+
+static nir_ssa_def *
+ptr_to_offset(nir_builder *b, nir_ssa_def *ptr)
+{
+   return nir_iand(b, ptr, nir_imm_int(b, 0x0ffffffc));
+}
+
+static void
+lower_load_global(nir_builder *b, nir_intrinsic_instr *intr)
+{
+   b->cursor = nir_before_instr(&intr->instr);
+
+   assert(intr->num_components == 1); // no support for vectors
+   assert(nir_dest_bit_size(intr->dest) == 32);
+
+   /* source 'pointer' */
+   assert(intr->src[0].is_ssa);
+   nir_ssa_def *ptr = intr->src[0].ssa;
+   nir_ssa_def *buffer = ptr_to_buffer(b, ptr);
+   nir_ssa_def *offset = ptr_to_offset(b, ptr);
+
+   nir_ssa_def *result;
+   nir_intrinsic_instr *load =
+      nir_intrinsic_instr_create(b->shader,
+                                 nir_intrinsic_load_global_dxil);
+   load->num_components = 1;
+
+   assert(intr->dest.is_ssa);
+   load->src[0] = nir_src_for_ssa(buffer);
+   load->src[1] = nir_src_for_ssa(offset);
+   nir_ssa_dest_init(&load->instr, &load->dest, load->num_components,
+                     32, intr->dest.ssa.name);
+   nir_builder_instr_insert(b, &load->instr);
+
+   result = &load->dest.ssa;
+   nir_ssa_def_rewrite_uses(&intr->dest.ssa,
+                            nir_src_for_ssa(result));
+   nir_instr_remove(&intr->instr);
+}
+
+static void
+lower_store_global(nir_builder *b, nir_intrinsic_instr *intr)
+{
+   b->cursor = nir_before_instr(&intr->instr);
+
+   assert(intr->num_components == 1); // no support for vectors
+   assert(nir_src_bit_size(intr->src[0]) == 32);
+
+   /* source 'pointer' */
+   assert(intr->src[1].is_ssa);
+   nir_ssa_def *ptr = intr->src[1].ssa;
+   nir_ssa_def *buffer = ptr_to_buffer(b, ptr);
+   nir_ssa_def *offset = ptr_to_offset(b, ptr);
+
+   assert(intr->src[0].is_ssa);
+   nir_ssa_def *value = intr->src[0].ssa;
+   nir_intrinsic_instr *store =
+      nir_intrinsic_instr_create(b->shader,
+                                 nir_intrinsic_store_global_dxil);
+   store->num_components = 1;
+   store->src[0] = nir_src_for_ssa(value);
+   store->src[1] = nir_src_for_ssa(buffer);
+   store->src[2] = nir_src_for_ssa(offset);
+   nir_builder_instr_insert(b, &store->instr);
+   nir_instr_remove(&intr->instr);
+}
+
+static void
+lower_global_mem_to_dxil(struct nir_shader *nir)
+{
+   foreach_list_typed(nir_function, func, node, &nir->functions) {
+      if (!func->is_entrypoint)
+         continue;
+      assert(func->impl);
+
+      nir_builder b;
+      nir_builder_init(&b, func->impl);
+
+      nir_foreach_block(block, func->impl) {
+         nir_foreach_instr_safe(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+            nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
+
+            switch (intr->intrinsic) {
+            case nir_intrinsic_load_kernel_input:
+               lower_load_kernel_input(&b, intr);
+               break;
+            case nir_intrinsic_load_global:
+               lower_load_global(&b, intr);
+               break;
+            case nir_intrinsic_store_global:
+               lower_store_global(&b, intr);
+               break;
+            }
+         }
+      }
+   }
+}
 
 enum clc_debug_flags {
    CLC_DEBUG_DUMP_SPIRV = 1 << 0,
@@ -287,6 +417,8 @@ clc_to_dxil(const struct clc_object *obj,
       NIR_PASS_V(nir, nir_lower_int64, nir_options->lower_int64_options);
 
    NIR_PASS_V(nir, nir_opt_dce);
+
+   NIR_PASS_V(nir, lower_global_mem_to_dxil);
 
    NIR_PASS_V(nir, nir_lower_bit_size, lower_bit_size_callback, NULL);
 

src/microsoft/compiler/nir_to_dxil.c
@@ -1768,27 +1768,6 @@ get_int32_undef(struct dxil_module *m)
 }
 
 static const struct dxil_value *
-ptr_to_buffer(struct dxil_module *m, const struct dxil_value *ptr)
-{
-   const struct dxil_value *shift = dxil_module_get_int32_const(m, 28);
-   if (!shift)
-      return NULL;
-
-   return dxil_emit_binop(m, DXIL_BINOP_LSHR, ptr, shift, 0);
-}
-
-static const struct dxil_value *
-ptr_to_offset(struct dxil_module *m, const struct dxil_value *ptr)
-{
-   const struct dxil_value *mask =
-      dxil_module_get_int32_const(m, (1u << 28) - 1);
-   if (!mask)
-      return NULL;
-
-   return dxil_emit_binop(m, DXIL_BINOP_AND, ptr, mask, 0);
-}
-
-static const struct dxil_value *
 offset_to_index(struct dxil_module *m, const struct dxil_value *offset,
                 unsigned bit_size)
 {
@@ -1818,15 +1797,15 @@ static bool
 emit_load_global(struct ntd_context *ctx, nir_intrinsic_instr *intr)
 {
    const struct dxil_value *int32_undef = get_int32_undef(&ctx->mod);
-   const struct dxil_value *ptr =
+   const struct dxil_value *buffer =
       get_src(ctx, &intr->src[0], 0, nir_type_uint);
-   if (!int32_undef || !ptr)
+   const struct dxil_value *offset =
+      get_src(ctx, &intr->src[1], 0, nir_type_uint);
+   if (!int32_undef || !buffer || !offset)
       return false;
 
-   const struct dxil_value *buffer = ptr_to_buffer(&ctx->mod, ptr);
-   const struct dxil_value *offset = ptr_to_offset(&ctx->mod, ptr);
-   if (!buffer || !offset)
-      return false;
+   assert(nir_src_bit_size(intr->src[0]) == 32);
+   assert(nir_intrinsic_dest_components(intr) <= 4);
 
    const struct dxil_value *handle =
       emit_createhandle_call(ctx, DXIL_RESOURCE_CLASS_UAV, 0, buffer, true);
@@ -1842,26 +1821,23 @@ emit_load_global(struct ntd_context *ctx, nir_intrinsic_instr *intr)
    if (!load)
       return false;
 
-   const struct dxil_value *val =
-      dxil_emit_extractval(&ctx->mod, load, 0);
-   if (!val)
-      return false;
-
-   store_dest_int(ctx, &intr->dest, 0, val);
+   for (int i = 0; i < nir_intrinsic_dest_components(intr); i++) {
+      const struct dxil_value *val =
+         dxil_emit_extractval(&ctx->mod, load, i);
+      if (!val)
+         return false;
+      store_dest_int(ctx, &intr->dest, i, val);
+   }
    return true;
 }
 
 static bool
 emit_store_global(struct ntd_context *ctx, nir_intrinsic_instr *intr)
 {
-   const struct dxil_value *int32_undef = get_int32_undef(&ctx->mod);
-   const struct dxil_value *ptr =
+   const struct dxil_value *buffer =
       get_src(ctx, &intr->src[1], 0, nir_type_uint);
-   if (!int32_undef || !ptr)
-      return false;
-
-   const struct dxil_value *buffer = ptr_to_buffer(&ctx->mod, ptr);
-   const struct dxil_value *offset = ptr_to_offset(&ctx->mod, ptr);
+   const struct dxil_value *offset =
+      get_src(ctx, &intr->src[2], 0, nir_type_uint);
    if (!buffer || !offset)
       return false;
 
@@ -1870,28 +1846,30 @@ emit_store_global(struct ntd_context *ctx, nir_intrinsic_instr *intr)
    if (!handle)
       return false;
 
+   assert(nir_src_bit_size(intr->src[0]) == 32);
+   unsigned num_components = nir_src_num_components(intr->src[0]);
+   assert(num_components <= 4);
+   const struct dxil_value *value[4];
+   for (unsigned i = 0; i < num_components; ++i) {
+      value[i] = get_src(ctx, &intr->src[0], i, nir_type_uint);
+      if (!value[i])
+         return false;
+   }
+
+   const struct dxil_value *int32_undef = get_int32_undef(&ctx->mod);
+   if (!int32_undef)
+      return false;
+
    const struct dxil_value *coord[2] = {
       offset,
       int32_undef
    };
 
-   uint32_t writemask = nir_intrinsic_write_mask(intr);
-   assert(writemask == 0x1 || writemask == 0x3 ||
-          writemask == 0x7 || writemask == 0xf);
-
-   const struct dxil_value *value[4];
-   for (unsigned i = 0; i < 4; ++i) {
-      if (writemask & (1 << i))
-         value[i] = get_src(ctx, &intr->src[0], i, nir_type_uint);
-      else
-         value[i] = int32_undef;
-
-      if (!value[i])
-         return false;
-   }
+   for (int i = num_components; i < 4; ++i)
+      value[i] = int32_undef;
 
    const struct dxil_value *write_mask =
-      dxil_module_get_int8_const(&ctx->mod, writemask);
+      dxil_module_get_int8_const(&ctx->mod, (1u << num_components) - 1);
    if (!write_mask)
       return false;
 
@@ -2228,11 +2206,9 @@ emit_intrinsic(struct ntd_context *ctx, nir_intrinsic_instr *intr)
       return emit_load_local_invocation_id(ctx, intr);
    case nir_intrinsic_load_work_group_id:
       return emit_load_local_work_group_id(ctx, intr);
-   case nir_intrinsic_load_kernel_input:
-      return emit_load_kernel_input(ctx, intr);
-   case nir_intrinsic_load_global:
+   case nir_intrinsic_load_global_dxil:
       return emit_load_global(ctx, intr);
-   case nir_intrinsic_store_global:
+   case nir_intrinsic_store_global_dxil:
       return emit_store_global(ctx, intr);
    case nir_intrinsic_store_deref:
       return emit_store_deref(ctx, intr);

src/compiler/nir/nir_intrinsics.py
@@ -841,6 +841,13 @@ intrinsic("store_global_ir3", [0, 2, 1], indices=[WRMASK, ACCESS, ALIGN_MUL, ALI
 # const_index[] = { access, align_mul, align_offset }
 intrinsic("load_global_ir3", [2, 1], dest_comp=0, indices=[ACCESS, ALIGN_MUL, ALIGN_OFFSET], flags=[CAN_ELIMINATE])
 
+# DXIL specific load/store intrinsics. These require index and offset instead
+# of address, due to lack of real pointers to global memory.
+# src[] = { value, index, offset }.
+intrinsic("store_global_dxil", [0, 1, 1])
+# src[] = { index, offset }.
+load("global_dxil", 2, [], [CAN_ELIMINATE, CAN_REORDER])
+
 # Intrinsics used by the Midgard/Bifrost blend pipeline. These are defined
 # within a blend shader to read/write the raw value from the tile buffer,
 # without applying any format conversion in the process. If the shader needs

commit 7fd0cfcfbb92d0451d3e8b5ecbaa839e687be826

microsoft/compiler: Don't lower load_kernel_input
We actually need to load things from the kernel_inputs CBV if we want
args passed by value to work properly.

Fixes: 802eed03579 ("microsoft/compiler: lower global load/store to dxil intrinsics")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

src/microsoft/clc/clc_compiler.c
@@ -40,20 +40,6 @@
  * want to unify that.
  */
 
-static void
-lower_load_kernel_input(nir_builder *b, nir_intrinsic_instr *intr)
-{
-   b->cursor = nir_before_instr(&intr->instr);
-
-   nir_src *src = &intr->src[0]; /* param location i.e. SSBO slot */
-   assert(src->is_ssa);
-
-   nir_ssa_def *ssbo_loc = nir_ishl(b, src->ssa, nir_imm_int(b, 28 - 2));
-   nir_ssa_def_rewrite_uses(&intr->dest.ssa,
-                            nir_src_for_ssa(ssbo_loc));
-   nir_instr_remove(&intr->instr);
-}
-
 static nir_ssa_def *
 ptr_to_buffer(nir_builder *b, nir_ssa_def *ptr)
 {
@@ -243,9 +229,6 @@ lower_global_mem_to_dxil(struct nir_shader *nir)
             nir_intrinsic_instr *intr = nir_instr_as_intrinsic(instr);
 
             switch (intr->intrinsic) {
-            case nir_intrinsic_load_kernel_input:
-               lower_load_kernel_input(&b, intr);
-               break;
             case nir_intrinsic_load_global:
                lower_load_global(&b, intr);
                break;

src/microsoft/compiler/nir_to_dxil.c
@@ -2297,6 +2297,8 @@ emit_intrinsic(struct ntd_context *ctx, nir_intrinsic_instr *intr)
       return emit_load_local_invocation_id(ctx, intr);
    case nir_intrinsic_load_work_group_id:
       return emit_load_local_work_group_id(ctx, intr);
+   case nir_intrinsic_load_kernel_input:
+      return emit_load_kernel_input(ctx, intr);
    case nir_intrinsic_load_global_dxil:
       return emit_load_global(ctx, intr);
    case nir_intrinsic_store_global_dxil: