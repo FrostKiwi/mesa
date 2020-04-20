FrostKiwi/mesa

commit c0fa6b0440e9a7fde45c624798f462073beaafb0

d3d12: Cache pipeline state objects

src/gallium/drivers/d3d12/d3d12_pipeline_state.cpp
@@ -1,0 +1,240 @@
+/*
+ * Copyright © Microsoft Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "d3d12_pipeline_state.h"
+#include "d3d12_compiler.h"
+#include "d3d12_context.h"
+#include "d3d12_screen.h"
+
+#include "util/hash_table.h"
+#include "util/set.h"
+#include "util/u_memory.h"
+#include "util/u_prim.h"
+
+struct d3d12_pso_entry {
+   struct d3d12_gfx_pipeline_state key;
+   ID3D12PipelineState *pso;
+};
+
+static bool
+depth_bias(struct d3d12_rasterizer_state *state, enum pipe_prim_type reduced_prim)
+{
+   switch (reduced_prim) {
+   case PIPE_PRIM_POINTS:
+      return state->base.offset_point;
+
+   case PIPE_PRIM_LINES:
+      return state->base.offset_line;
+
+   case PIPE_PRIM_TRIANGLES:
+      return state->base.offset_tri;
+
+   default:
+      unreachable("unexpected reduced prim");
+   }
+}
+
+static D3D12_PRIMITIVE_TOPOLOGY_TYPE
+topology_type(enum pipe_prim_type reduced_prim)
+{
+   switch (reduced_prim) {
+   case PIPE_PRIM_POINTS:
+      return D3D12_PRIMITIVE_TOPOLOGY_TYPE_POINT;
+
+   case PIPE_PRIM_LINES:
+      return D3D12_PRIMITIVE_TOPOLOGY_TYPE_LINE;
+
+   case PIPE_PRIM_TRIANGLES:
+      return D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+
+   case PIPE_PRIM_PATCHES:
+      return D3D12_PRIMITIVE_TOPOLOGY_TYPE_PATCH;
+
+   default:
+      debug_printf("pipe_prim_type: %s\n", u_prim_name(reduced_prim));
+      unreachable("unexpected enum pipe_prim_type");
+   }
+}
+
+static ID3D12PipelineState *
+create_gfx_pipeline_state(struct d3d12_context *ctx)
+{
+   struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+   struct d3d12_gfx_pipeline_state *state = &ctx->gfx_pipeline_state;
+   enum pipe_prim_type reduced_prim = u_reduced_prim(state->prim_type);
+
+   D3D12_GRAPHICS_PIPELINE_STATE_DESC pso_desc = { 0 };
+   pso_desc.pRootSignature = state->root_signature;
+
+   if (state->stages[PIPE_SHADER_VERTEX]) {
+      auto shader = state->stages[PIPE_SHADER_VERTEX];
+      pso_desc.VS.BytecodeLength = shader->bytecode_length;
+      pso_desc.VS.pShaderBytecode = shader->bytecode;
+   }
+
+   if (state->stages[PIPE_SHADER_FRAGMENT]) {
+      auto shader = state->stages[PIPE_SHADER_FRAGMENT];
+      pso_desc.PS.BytecodeLength = shader->bytecode_length;
+      pso_desc.PS.pShaderBytecode = shader->bytecode;
+   }
+
+   if (state->stages[PIPE_SHADER_GEOMETRY]) {
+      auto shader = state->stages[PIPE_SHADER_GEOMETRY];
+      pso_desc.GS.BytecodeLength = shader->bytecode_length;
+      pso_desc.GS.pShaderBytecode = shader->bytecode;
+   }
+
+   pso_desc.BlendState = state->blend->desc;
+   pso_desc.DepthStencilState = state->zsa->desc;
+   pso_desc.SampleMask = state->sample_mask;
+   pso_desc.RasterizerState = state->rast->desc;
+
+   if (reduced_prim != PIPE_PRIM_TRIANGLES)
+      pso_desc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
+
+   if (depth_bias(state->rast, reduced_prim)) {
+      pso_desc.RasterizerState.DepthBias = state->rast->base.offset_units;
+      pso_desc.RasterizerState.DepthBiasClamp = state->rast->base.offset_clamp;
+      pso_desc.RasterizerState.SlopeScaledDepthBias = state->rast->base.offset_scale;
+   }
+
+   pso_desc.InputLayout.pInputElementDescs = state->ves->elements;
+   pso_desc.InputLayout.NumElements = state->ves->num_elements;
+
+   pso_desc.IBStripCutValue = D3D12_INDEX_BUFFER_STRIP_CUT_VALUE_DISABLED; // TODO
+
+   pso_desc.PrimitiveTopologyType = topology_type(reduced_prim);
+
+   pso_desc.NumRenderTargets = ctx->fb.nr_cbufs;
+   for (int i = 0; i < ctx->fb.nr_cbufs; ++i)
+      pso_desc.RTVFormats[i] = state->rtv_formats[i];
+   pso_desc.DSVFormat = state->dsv_format;
+
+   pso_desc.SampleDesc.Count = 1;
+   pso_desc.SampleDesc.Quality = 0;
+
+   pso_desc.NodeMask = 0;
+
+   pso_desc.CachedPSO.pCachedBlob = NULL;
+   pso_desc.CachedPSO.CachedBlobSizeInBytes = 0;
+
+   pso_desc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
+
+   ID3D12PipelineState *ret;
+   if (FAILED(screen->dev->CreateGraphicsPipelineState(&pso_desc,
+                                                       __uuidof(ret),
+                                                       (void **)&ret))) {
+      debug_printf("D3D12: CreateGraphicsPipelineState failed!\n");
+      return NULL;
+   }
+
+   return ret;
+}
+
+static uint32_t
+hash_gfx_pipeline_state(const void *key)
+{
+   return _mesa_hash_data(key, sizeof(struct d3d12_gfx_pipeline_state));
+}
+
+static bool
+equals_gfx_pipeline_state(const void *a, const void *b)
+{
+   return memcmp(a, b, sizeof(struct d3d12_gfx_pipeline_state)) == 0;
+}
+
+ID3D12PipelineState *
+d3d12_get_gfx_pipeline_state(struct d3d12_context *ctx)
+{
+   uint32_t hash = hash_gfx_pipeline_state(&ctx->gfx_pipeline_state);
+   struct hash_entry *entry = _mesa_hash_table_search_pre_hashed(ctx->pso_cache, hash,
+                                                                 &ctx->gfx_pipeline_state);
+   if (!entry) {
+      struct d3d12_pso_entry *data = (struct d3d12_pso_entry *)MALLOC(sizeof(struct d3d12_pso_entry));
+      if (!data)
+         return NULL;
+
+      data->key = ctx->gfx_pipeline_state;
+      data->pso = create_gfx_pipeline_state(ctx);
+      if (!data->pso)
+         return NULL;
+
+      entry = _mesa_hash_table_insert_pre_hashed(ctx->pso_cache, hash, &data->key, data);
+      assert(entry);
+   }
+
+   return ((struct d3d12_pso_entry *)(entry->data))->pso;
+}
+
+void
+d3d12_gfx_pipeline_state_cache_init(struct d3d12_context *ctx)
+{
+   ctx->pso_cache = _mesa_hash_table_create(NULL, NULL, equals_gfx_pipeline_state);
+}
+
+static void
+delete_entry(struct hash_entry *entry)
+{
+   struct d3d12_pso_entry *data = (struct d3d12_pso_entry *)entry->data;
+   data->pso->Release();
+   FREE(data);
+}
+
+void
+d3d12_gfx_pipeline_state_cache_destroy(struct d3d12_context *ctx)
+{
+   _mesa_hash_table_destroy(ctx->pso_cache, delete_entry);
+}
+
+void
+d3d12_gfx_pipeline_state_cache_invalidate(struct d3d12_context *ctx, const void *state)
+{
+   hash_table_foreach(ctx->pso_cache, entry) {
+      const struct d3d12_gfx_pipeline_state *key = (struct d3d12_gfx_pipeline_state *)entry->key;
+      if (key->blend == state || key->zsa == state || key->rast == state) {
+         if (ctx->current_pso == (ID3D12PipelineState *)entry->data)
+            ctx->current_pso = NULL;
+         _mesa_hash_table_remove(ctx->pso_cache, entry);
+      }
+   }
+}
+
+void
+d3d12_gfx_pipeline_state_cache_invalidate_shader(struct d3d12_context *ctx,
+                                                 enum pipe_shader_type stage,
+                                                 struct d3d12_shader_selector *selector)
+{
+   struct d3d12_shader *shader = selector->first;
+
+   while (shader) {
+      hash_table_foreach(ctx->pso_cache, entry) {
+         const struct d3d12_gfx_pipeline_state *key = (struct d3d12_gfx_pipeline_state *)entry->key;
+         if (key->stages[stage] == shader) {
+            if (ctx->current_pso == (ID3D12PipelineState *)entry->data)
+               ctx->current_pso = NULL;
+            _mesa_hash_table_remove(ctx->pso_cache, entry);
+         }
+      }
+      shader = shader->next_variant;
+   }
+}

src/gallium/drivers/d3d12/d3d12_batch.cpp
@@ -165,7 +165,7 @@ d3d12_start_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
    ctx->cmdlist->SetDescriptorHeaps(2, heaps);
    ctx->cmdlist_dirty = ~0;
    for (int i = 0; i < D3D12_GFX_SHADER_STAGES; ++i)
-      ctx->shader_state[i].state_dirty = ~0;
+      ctx->shader_dirty[i] = ~0;
 
    if (!ctx->queries_disabled)
       d3d12_resume_queries(ctx);

src/gallium/drivers/d3d12/d3d12_blit.cpp
@@ -42,11 +42,11 @@ d3d12_blit(struct pipe_context *pctx,
       return;
    }
 
-   util_blitter_save_blend(ctx->blitter, ctx->blend);
-   util_blitter_save_depth_stencil_alpha(ctx->blitter, ctx->depth_stencil_alpha_state);
-   util_blitter_save_vertex_elements(ctx->blitter, ctx->ves);
+   util_blitter_save_blend(ctx->blitter, ctx->gfx_pipeline_state.blend);
+   util_blitter_save_depth_stencil_alpha(ctx->blitter, ctx->gfx_pipeline_state.zsa);
+   util_blitter_save_vertex_elements(ctx->blitter, ctx->gfx_pipeline_state.ves);
    util_blitter_save_stencil_ref(ctx->blitter, &ctx->stencil_ref);
-   util_blitter_save_rasterizer(ctx->blitter, ctx->rast);
+   util_blitter_save_rasterizer(ctx->blitter, ctx->gfx_pipeline_state.rast);
    util_blitter_save_fragment_shader(ctx->blitter, ctx->gfx_stages[PIPE_SHADER_FRAGMENT]);
    util_blitter_save_vertex_shader(ctx->blitter, ctx->gfx_stages[PIPE_SHADER_VERTEX]);
    util_blitter_save_geometry_shader(ctx->blitter, ctx->gfx_stages[PIPE_SHADER_GEOMETRY]);
@@ -61,7 +61,7 @@ d3d12_blit(struct pipe_context *pctx,
                                             ctx->sampler_views[PIPE_SHADER_FRAGMENT]);
    util_blitter_save_fragment_constant_buffer_slot(ctx->blitter, ctx->cbufs[PIPE_SHADER_FRAGMENT]);
    util_blitter_save_vertex_buffer_slot(ctx->blitter, ctx->vbs);
-   util_blitter_save_sample_mask(ctx->blitter, ctx->sample_mask);
+   util_blitter_save_sample_mask(ctx->blitter, ctx->gfx_pipeline_state.sample_mask);
 
    util_blitter_blit(ctx->blitter, info);
 }

src/gallium/drivers/d3d12/d3d12_compiler.cpp
@@ -274,7 +274,7 @@ needs_point_sprite_lowering(struct d3d12_context *ctx, const struct pipe_draw_in
    } else {
       /* No user GS; check if we are drawing wide points */
       return (dinfo->mode == PIPE_PRIM_POINTS &&
-              (ctx->rast->base.point_size > 1.0 ||
+              (ctx->gfx_pipeline_state.rast->base.point_size > 1.0 ||
                vs->first->nir->info.outputs_written & VARYING_SLOT_PSIZ));
    }
 }
@@ -416,11 +416,11 @@ d3d12_fill_shader_key(struct d3d12_selection_context *sel_ctx,
    }
 
    if (stage == PIPE_SHADER_GEOMETRY && sel_ctx->needs_point_sprite_lowering) {
+      struct pipe_rasterizer_state *rast = &sel_ctx->ctx->gfx_pipeline_state.rast->base;
       key->gs.writes_psize = 1;
-      key->gs.sprite_coord_enable = sel_ctx->ctx->rast->base.sprite_coord_enable;
-      key->gs.sprite_origin_upper_left =
-         (sel_ctx->ctx->rast->base.sprite_coord_mode != PIPE_SPRITE_COORD_LOWER_LEFT);
-      key->gs.aa_point = sel_ctx->ctx->rast->base.point_smooth;
+      key->gs.sprite_coord_enable = rast->sprite_coord_enable;
+      key->gs.sprite_origin_upper_left = (rast->sprite_coord_mode != PIPE_SPRITE_COORD_LOWER_LEFT);
+      key->gs.aa_point = rast->point_smooth;
    }
 }
 

src/gallium/drivers/d3d12/d3d12_context.cpp
@@ -64,6 +64,7 @@ d3d12_context_destroy(struct pipe_context *pctx)
    d3d12_descriptor_pool_free(ctx->view_pool);
    util_primconvert_destroy(ctx->primconvert);
    slab_destroy_child(&ctx->transfer_pool);
+   d3d12_gfx_pipeline_state_cache_destroy(ctx);
 
    u_suballocator_destroy(ctx->query_allocator);
 
@@ -110,7 +111,7 @@ d3d12_bind_vertex_elements_state(struct pipe_context *pctx,
                                  void *ve)
 {
    struct d3d12_context *ctx = d3d12_context(pctx);
-   ctx->ves = (struct d3d12_vertex_elements_state *)ve;
+   ctx->gfx_pipeline_state.ves = (struct d3d12_vertex_elements_state *)ve;
    ctx->state_dirty |= D3D12_DIRTY_VERTEX_ELEMENTS;
 }
 
@@ -330,9 +331,9 @@ d3d12_bind_blend_state(struct pipe_context *pctx, void *blend_state)
 {
    struct d3d12_context *ctx = d3d12_context(pctx);
    struct d3d12_blend_state *new_state = (struct d3d12_blend_state *) blend_state;
-   struct d3d12_blend_state *old_state = ctx->blend;
+   struct d3d12_blend_state *old_state = ctx->gfx_pipeline_state.blend;
 
-   ctx->blend = new_state;
+   ctx->gfx_pipeline_state.blend = new_state;
    ctx->state_dirty |= D3D12_DIRTY_BLEND;
    if (new_state == NULL || old_state == NULL ||
        new_state->blend_factor_flags != old_state->blend_factor_flags)
@@ -342,6 +343,7 @@ d3d12_bind_blend_state(struct pipe_context *pctx, void *blend_state)
 static void
 d3d12_delete_blend_state(struct pipe_context *pctx, void *blend_state)
 {
+   d3d12_gfx_pipeline_state_cache_invalidate(d3d12_context(pctx), blend_state);
    FREE(blend_state);
 }
 
@@ -416,7 +418,7 @@ d3d12_bind_depth_stencil_alpha_state(struct pipe_context *pctx,
                                      void *dsa)
 {
    struct d3d12_context *ctx = d3d12_context(pctx);
-   ctx->depth_stencil_alpha_state = (struct d3d12_depth_stencil_alpha_state *) dsa;
+   ctx->gfx_pipeline_state.zsa = (struct d3d12_depth_stencil_alpha_state *) dsa;
    ctx->state_dirty |= D3D12_DIRTY_ZSA;
 }
 
@@ -424,6 +426,7 @@ static void
 d3d12_delete_depth_stencil_alpha_state(struct pipe_context *pctx,
                                        void *dsa_state)
 {
+   d3d12_gfx_pipeline_state_cache_invalidate(d3d12_context(pctx), dsa_state);
    FREE(dsa_state);
 }
 
@@ -492,13 +495,14 @@ static void
 d3d12_bind_rasterizer_state(struct pipe_context *pctx, void *rs_state)
 {
    struct d3d12_context *ctx = d3d12_context(pctx);
-   ctx->rast = (struct d3d12_rasterizer_state *)rs_state;
+   ctx->gfx_pipeline_state.rast = (struct d3d12_rasterizer_state *)rs_state;
    ctx->state_dirty |= D3D12_DIRTY_RASTERIZER;
 }
 
 static void
 d3d12_delete_rasterizer_state(struct pipe_context *pctx, void *rs_state)
 {
+   d3d12_gfx_pipeline_state_cache_invalidate(d3d12_context(pctx), rs_state);
    FREE(rs_state);
 }
 
@@ -614,7 +618,7 @@ d3d12_bind_sampler_states(struct pipe_context *pctx,
       ctx->samplers[shader][start_slot + i] = sampler;
    }
    ctx->num_samplers[shader] = start_slot + num_samplers;
-   ctx->shader_state[shader].state_dirty |= D3D12_SHADER_DIRTY_SAMPLERS;
+   ctx->shader_dirty[shader] |= D3D12_SHADER_DIRTY_SAMPLERS;
 }
 
 static void
@@ -731,7 +735,7 @@ d3d12_set_sampler_views(struct pipe_context *pctx,
          views[i]);
    }
    ctx->num_sampler_views[shader_type] = start_slot + num_views;
-   ctx->shader_state[shader_type].state_dirty |= D3D12_SHADER_DIRTY_SAMPLER_VIEWS;
+   ctx->shader_dirty[shader_type] |= D3D12_SHADER_DIRTY_SAMPLER_VIEWS;
 }
 
 static void
@@ -746,12 +750,20 @@ d3d12_destroy_sampler_view(struct pipe_context *pctx,
 }
 
 static void
+delete_shader(struct d3d12_context *ctx, enum pipe_shader_type stage,
+              struct d3d12_shader_selector *shader)
+{
+   d3d12_gfx_pipeline_state_cache_invalidate_shader(ctx, stage, shader);
+   d3d12_shader_free(shader);
+}
+
+static void
 bind_stage(struct d3d12_context *ctx, enum pipe_shader_type stage,
            struct d3d12_shader_selector *shader)
 {
    assert(stage < D3D12_GFX_SHADER_STAGES);
    if (ctx->gfx_stages[stage] && ctx->gfx_stages[stage]->passthrough)
-      d3d12_shader_free(shader);
+      delete_shader(ctx, stage, ctx->gfx_stages[stage]);
    ctx->gfx_stages[stage] = shader;
 }
 
@@ -774,7 +786,8 @@ static void
 d3d12_delete_vs_state(struct pipe_context *pctx,
                       void *vs)
 {
-   d3d12_shader_free((struct d3d12_shader_selector *) vs);
+   delete_shader(d3d12_context(pctx), PIPE_SHADER_VERTEX,
+                 (struct d3d12_shader_selector *) vs);
 }
 
 static void *
@@ -796,7 +809,8 @@ static void
 d3d12_delete_fs_state(struct pipe_context *pctx,
                       void *fs)
 {
-   d3d12_shader_free((struct d3d12_shader_selector *) fs);
+   delete_shader(d3d12_context(pctx), PIPE_SHADER_FRAGMENT,
+                 (struct d3d12_shader_selector *) fs);
 }
 
 static void *
@@ -816,7 +830,8 @@ d3d12_bind_gs_state(struct pipe_context *pctx, void *gss)
 static void
 d3d12_delete_gs_state(struct pipe_context *pctx, void *gs)
 {
-   d3d12_shader_free((struct d3d12_shader_selector *) gs);
+   delete_shader(d3d12_context(pctx), PIPE_SHADER_GEOMETRY,
+                 (struct d3d12_shader_selector *) gs);
 }
 
 static void
@@ -921,7 +936,7 @@ d3d12_set_constant_buffer(struct pipe_context *pctx,
       ctx->cbufs[shader][index].buffer_size = 0;
       ctx->cbufs[shader][index].user_buffer = NULL;
    }
-   ctx->shader_state[shader].state_dirty |= D3D12_SHADER_DIRTY_CONSTBUF;
+   ctx->shader_dirty[shader] |= D3D12_SHADER_DIRTY_CONSTBUF;
 }
 
 static void
@@ -931,6 +946,16 @@ d3d12_set_framebuffer_state(struct pipe_context *pctx,
    struct d3d12_context *ctx = d3d12_context(pctx);
 
    util_copy_framebuffer_state(&d3d12_context(pctx)->fb, state);
+
+   ctx->gfx_pipeline_state.num_cbufs = state->nr_cbufs;
+   for (int i = 0; i < state->nr_cbufs; ++i)
+      ctx->gfx_pipeline_state.rtv_formats[i] = d3d12_get_format(state->cbufs[i]->format);
+
+   if (state->zsbuf)
+      ctx->gfx_pipeline_state.dsv_format = d3d12_get_format(state->zsbuf->format);
+   else
+      ctx->gfx_pipeline_state.dsv_format = DXGI_FORMAT_UNKNOWN;
+
    ctx->state_dirty |= D3D12_DIRTY_FRAMEBUFFER;
 }
 
@@ -947,7 +972,7 @@ static void
 d3d12_set_sample_mask(struct pipe_context *pctx, unsigned sample_mask)
 {
    struct d3d12_context *ctx = d3d12_context(pctx);
-   ctx->sample_mask = sample_mask;
+   ctx->gfx_pipeline_state.sample_mask = sample_mask;
    ctx->state_dirty |= D3D12_DIRTY_SAMPLE_MASK;
 }
 
@@ -1240,7 +1265,7 @@ d3d12_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
    ctx->base.flush = d3d12_flush;
    ctx->base.blit = d3d12_blit;
 
-   ctx->sample_mask = ~0;
+   ctx->gfx_pipeline_state.sample_mask = ~0;
 
    d3d12_context_surface_init(&ctx->base);
    d3d12_context_resource_init(&ctx->base);
@@ -1262,6 +1287,8 @@ d3d12_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
       debug_printf("D3D12: failed to create primconvert\n");
       return NULL;
    }
+
+   d3d12_gfx_pipeline_state_cache_init(ctx);
 
    HMODULE hD3D12Mod = LoadLibrary("D3D12.DLL");
    if (!hD3D12Mod) {

src/gallium/drivers/d3d12/d3d12_context.h
@@ -26,6 +26,7 @@
 
 #include "d3d12_batch.h"
 #include "d3d12_descriptor_pool.h"
+#include "d3d12_pipeline_state.h"
 
 #include "pipe/p_context.h"
 #include "pipe/p_state.h"
@@ -90,16 +91,6 @@ enum resource_dimension
    RESOURCE_DIMENSION_COUNT
 };
 
-struct d3d12_vertex_elements_state {
-   D3D12_INPUT_ELEMENT_DESC elements[PIPE_MAX_ATTRIBS];
-   unsigned num_elements;
-};
-
-struct d3d12_rasterizer_state {
-   struct pipe_rasterizer_state base;
-   D3D12_RASTERIZER_DESC desc;
-};
-
 struct d3d12_sampler_state {
    D3D12_STATIC_SAMPLER_DESC desc;
    struct d3d12_descriptor_handle handle;
@@ -110,15 +101,6 @@ enum d3d12_blend_factor_flags {
    D3D12_BLEND_FACTOR_COLOR = 1 << 0,
    D3D12_BLEND_FACTOR_ALPHA = 1 << 1,
    D3D12_BLEND_FACTOR_ANY   = 1 << 2,
-};
-
-struct d3d12_blend_state {
-   D3D12_BLEND_DESC desc;
-   unsigned blend_factor_flags;
-};
-
-struct d3d12_depth_stencil_alpha_state {
-   D3D12_DEPTH_STENCIL_DESC desc;
 };
 
 struct d3d12_sampler_view {
@@ -147,13 +129,13 @@ struct d3d12_context {
    struct primconvert_context *primconvert;
    struct blitter_context *blitter;
    struct u_suballocator *query_allocator;
+   struct hash_table *pso_cache;
 
    struct d3d12_batch batches[4];
    unsigned current_batch_idx;
 
    struct pipe_constant_buffer cbufs[PIPE_SHADER_TYPES][PIPE_MAX_CONSTANT_BUFFERS];
    struct pipe_framebuffer_state fb;
-   struct d3d12_vertex_elements_state *ves;
    struct pipe_vertex_buffer vbs[PIPE_MAX_ATTRIBS];
    D3D12_VERTEX_BUFFER_VIEW vbvs[PIPE_MAX_ATTRIBS];
    unsigned num_vbs;
@@ -164,25 +146,20 @@ struct d3d12_context {
    struct pipe_scissor_state scissor_states[PIPE_MAX_VIEWPORTS];
    D3D12_RECT scissors[PIPE_MAX_VIEWPORTS];
    unsigned num_scissors;
-   struct d3d12_blend_state *blend;
    float blend_factor[4];
-   unsigned sample_mask;
-   struct d3d12_depth_stencil_alpha_state *depth_stencil_alpha_state;
    struct pipe_stencil_ref stencil_ref;
-   struct d3d12_rasterizer_state *rast;
    struct pipe_sampler_view *sampler_views[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_SAMPLER_VIEWS];
    unsigned num_sampler_views[PIPE_SHADER_TYPES];
    struct d3d12_sampler_state *samplers[PIPE_SHADER_TYPES][PIPE_MAX_SHADER_SAMPLER_VIEWS];
    unsigned num_samplers[PIPE_SHADER_TYPES];
-   enum pipe_prim_type prim_type;
    D3D12_INDEX_BUFFER_VIEW ibv;
 
    struct d3d12_shader_selector *gfx_stages[D3D12_GFX_SHADER_STAGES];
 
-   struct d3d12_shader_state shader_state[D3D12_GFX_SHADER_STAGES];
+   struct d3d12_gfx_pipeline_state gfx_pipeline_state;
+   unsigned shader_dirty[D3D12_GFX_SHADER_STAGES];
    unsigned state_dirty;
    unsigned cmdlist_dirty;
-   ID3D12RootSignature *current_root_signature;
    ID3D12PipelineState *current_pso;
 
    ID3D12Fence *cmdqueue_fence;

commit eade7d8e0f081a642eaa703a7963f34f0eddb3e2

d3d12: delete PSO when removing it from the cache
Removing an entry from the hash table doesn't delete it,
so do it manually.

Fixes: c0fa6b0440e9a7fde45c624798f462073beaafb0
  d3d12: Cache pipeline state objects

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/gallium/drivers/d3d12/d3d12_pipeline_state.cpp
@@ -215,6 +215,7 @@ d3d12_gfx_pipeline_state_cache_invalidate(struct d3d12_context *ctx, const void 
          if (ctx->current_pso == (ID3D12PipelineState *)entry->data)
             ctx->current_pso = NULL;
          _mesa_hash_table_remove(ctx->pso_cache, entry);
+         delete_entry(entry);
       }
    }
 }
@@ -233,6 +234,7 @@ d3d12_gfx_pipeline_state_cache_invalidate_shader(struct d3d12_context *ctx,
             if (ctx->current_pso == (ID3D12PipelineState *)entry->data)
                ctx->current_pso = NULL;
             _mesa_hash_table_remove(ctx->pso_cache, entry);
+            delete_entry(entry);
          }
       }
       shader = shader->next_variant;