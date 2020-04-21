FrostKiwi/mesa

commit 9805bc528f10a76dcf3eb9db71abde6bada1327b

d3d12: Keep track of resources used in a batch

src/gallium/drivers/d3d12/d3d12_batch.cpp
@@ -25,14 +25,31 @@
 #include "d3d12_context.h"
 #include "d3d12_fence.h"
 #include "d3d12_query.h"
+#include "d3d12_resource.h"
 #include "d3d12_screen.h"
+#include "d3d12_surface.h"
 
+#include "util/hash_table.h"
+#include "util/set.h"
 #include "util/u_inlines.h"
 
 bool
 d3d12_init_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
 {
    struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
+
+   batch->sampler_views = _mesa_set_create(NULL, _mesa_hash_pointer,
+                                           _mesa_key_pointer_equal);
+   batch->surfaces = _mesa_set_create(NULL, _mesa_hash_pointer,
+                                      _mesa_key_pointer_equal);
+   batch->objects = _mesa_set_create(NULL,
+                                     _mesa_hash_pointer,
+                                     _mesa_key_pointer_equal);
+
+   if (!batch->sampler_views || !batch->surfaces || !batch->objects)
+      return false;
+
+   util_dynarray_init(&batch->zombie_samplers, NULL);
 
    if (FAILED(screen->dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
                                                   __uuidof(batch->cmdalloc),
@@ -59,6 +76,27 @@ d3d12_init_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
 }
 
 static void
+delete_sampler_view(set_entry *entry)
+{
+   struct pipe_sampler_view *pres = (struct pipe_sampler_view *)entry->key;
+   pipe_sampler_view_reference(&pres, NULL);
+}
+
+static void
+delete_surface(set_entry *entry)
+{
+   struct pipe_surface *surf = (struct pipe_surface *)entry->key;
+   pipe_surface_reference(&surf, NULL);
+}
+
+static void
+delete_object(set_entry *entry)
+{
+   ID3D12Object *object = (ID3D12Object *)entry->key;
+   object->Release();
+}
+
+static void
 reset_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
 {
    struct d3d12_screen *screen = d3d12_screen(ctx->base.screen);
@@ -69,6 +107,14 @@ reset_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
 
    d3d12_fence_finish(batch->fence, PIPE_TIMEOUT_INFINITE);
    d3d12_fence_reference(&batch->fence, NULL);
+
+   _mesa_set_clear(batch->sampler_views, delete_sampler_view);
+   _mesa_set_clear(batch->surfaces, delete_surface);
+   _mesa_set_clear(batch->objects, delete_object);
+
+   util_dynarray_foreach(&batch->zombie_samplers, d3d12_descriptor_handle, handle)
+      d3d12_descriptor_handle_free(handle);
+   util_dynarray_clear(&batch->zombie_samplers);
 
    d3d12_descriptor_heap_clear(batch->view_heap);
    d3d12_descriptor_heap_clear(batch->sampler_heap);
@@ -86,6 +132,9 @@ d3d12_destroy_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
    batch->cmdalloc->Release();
    d3d12_descriptor_heap_free(batch->sampler_heap);
    d3d12_descriptor_heap_free(batch->view_heap);
+   _mesa_set_destroy(batch->sampler_views, NULL);
+   _mesa_set_destroy(batch->surfaces, NULL);
+   _mesa_set_destroy(batch->objects, NULL);
 }
 
 void
@@ -135,4 +184,37 @@ d3d12_end_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
    ID3D12CommandList* cmdlists[] = { ctx->cmdlist };
    screen->cmdqueue->ExecuteCommandLists(1, cmdlists);
    batch->fence = d3d12_create_fence(screen, ctx);
+}
+
+void
+d3d12_batch_reference_sampler_view(struct d3d12_batch *batch,
+                                   struct d3d12_sampler_view *sv)
+{
+   struct set_entry *entry = _mesa_set_search(batch->sampler_views, sv);
+   if (!entry) {
+      entry = _mesa_set_add(batch->sampler_views, sv);
+      pipe_reference(NULL, &sv->base.reference);
+   }
+}
+
+void
+d3d12_batch_reference_surface(struct d3d12_batch *batch,
+                              struct d3d12_surface *surf)
+{
+   struct set_entry *entry = _mesa_set_search(batch->surfaces, surf);
+   if (!entry) {
+      entry = _mesa_set_add(batch->surfaces, surf);
+      pipe_reference(NULL, &surf->base.reference);
+   }
+}
+
+void
+d3d12_batch_reference_object(struct d3d12_batch *batch,
+                             ID3D12Object *object)
+{
+   struct set_entry *entry = _mesa_set_search(batch->objects, object);
+   if (!entry) {
+      entry = _mesa_set_add(batch->objects, object);
+      object->AddRef();
+   }
 }

src/gallium/drivers/d3d12/d3d12_context.cpp
@@ -606,9 +606,10 @@ static void
 d3d12_delete_sampler_state(struct pipe_context *pctx,
                            void *ss)
 {
-   struct d3d12_context *ctx = d3d12_context(pctx);
+   struct d3d12_batch *batch = d3d12_current_batch(d3d12_context(pctx));
    struct d3d12_sampler_state *state = (struct d3d12_sampler_state*) ss;
-   d3d12_descriptor_handle_free(&state->handle);
+   util_dynarray_append(&batch->zombie_samplers, d3d12_descriptor_handle,
+                        state->handle);
    FREE(ss);
 }
 

src/gallium/drivers/d3d12/d3d12_batch.h
@@ -24,6 +24,8 @@
 #ifndef D3D12_BATCH_H
 #define D3D12_BATCH_H
 
+#include "util/u_dynarray.h"
+
 #include <d3d12.h>
 
 struct d3d12_descriptor_heap;
@@ -31,6 +33,12 @@ struct d3d12_fence;
 
 struct d3d12_batch {
    struct d3d12_fence *fence;
+
+   struct set *sampler_views;
+   struct set *surfaces;
+   struct set *objects;
+
+   struct util_dynarray zombie_samplers;
 
    ID3D12CommandAllocator *cmdalloc;
    struct d3d12_descriptor_heap *sampler_heap;
@@ -48,5 +56,20 @@ d3d12_start_batch(struct d3d12_context *ctx, struct d3d12_batch *batch);
 
 void
 d3d12_end_batch(struct d3d12_context *ctx, struct d3d12_batch *batch);
+
+#define d3d12_batch_reference_resource(batch, resource) \
+   d3d12_batch_reference_object(batch, resource->res)
+
+void
+d3d12_batch_reference_sampler_view(struct d3d12_batch *batch,
+                                   struct d3d12_sampler_view *sv);
+
+void
+d3d12_batch_reference_surface(struct d3d12_batch *batch,
+                              struct d3d12_surface *surf);
+
+void
+d3d12_batch_reference_object(struct d3d12_batch *batch,
+                             ID3D12Object *object);
 
 #endif

src/gallium/drivers/d3d12/d3d12_draw.cpp
@@ -201,8 +201,8 @@ fill_cbv_descriptors(struct d3d12_context *ctx,
       struct d3d12_resource *res = d3d12_resource(buffer->buffer);
       D3D12_CONSTANT_BUFFER_VIEW_DESC cbv_desc = {};
       cbv_desc.BufferLocation = res->res->GetGPUVirtualAddress() + buffer->buffer_offset;
-
       cbv_desc.SizeInBytes = align(buffer->buffer_size, 256);
+      d3d12_batch_reference_resource(batch, res);
 
       struct d3d12_descriptor_handle handle;
       d3d12_descriptor_heap_alloc_handle(batch->view_heap, &handle);
@@ -229,10 +229,12 @@ fill_srv_descriptors(struct d3d12_context *ctx,
       struct d3d12_sampler_view *view =
          (struct d3d12_sampler_view*) ctx->sampler_views[stage][index];
 
-      if (view != NULL)
+      if (view != NULL) {
          descs[i] = view->handle.cpu_handle ;
-      else
+         d3d12_batch_reference_sampler_view(batch, view);
+      } else {
          descs[i] = ctx->null_srvs[shader->srv_bindings[i].dimension].cpu_handle;
+      }
    }
 
    d3d12_descriptor_heap_append_handles(batch->view_heap, descs, shader->num_srv_bindings);
@@ -497,6 +499,7 @@ d3d12_draw_vbo(struct pipe_context *pctx,
                const struct pipe_draw_info *dinfo)
 {
    struct d3d12_context *ctx = d3d12_context(pctx);
+   struct d3d12_batch *batch = d3d12_current_batch(ctx);
 
    d3d12_select_shader_variants(ctx, dinfo);
 
@@ -537,7 +540,9 @@ d3d12_draw_vbo(struct pipe_context *pctx,
       get_gfx_pipeline_state(ctx, root_sig, reduced_prim);
    assert(pipeline_state);
 
+   d3d12_batch_reference_object(batch, root_sig);
    ctx->cmdlist->SetGraphicsRootSignature(root_sig);
+   root_sig->Release();
 
    set_graphics_root_parameters(ctx);
 
@@ -553,7 +558,9 @@ d3d12_draw_vbo(struct pipe_context *pctx,
       ctx->cmdlist->RSSetScissorRects(1, &fb_scissor);
    }
 
+   d3d12_batch_reference_object(batch, pipeline_state);
    ctx->cmdlist->SetPipelineState(pipeline_state);
+   pipeline_state->Release();
 
    if (ctx->blend->blend_factor_flags & (D3D12_BLEND_FACTOR_COLOR | D3D12_BLEND_FACTOR_ANY)) {
       ctx->cmdlist->OMSetBlendFactor(ctx->blend_factor);
@@ -566,18 +573,27 @@ d3d12_draw_vbo(struct pipe_context *pctx,
 
    D3D12_CPU_DESCRIPTOR_HANDLE render_targets[PIPE_MAX_COLOR_BUFS] = {};
    D3D12_CPU_DESCRIPTOR_HANDLE *depth_desc = NULL, tmp_desc;
-   for (int i = 0; i < ctx->fb.nr_cbufs; ++i)
-      render_targets[i] = d3d12_surface(ctx->fb.cbufs[i])->desc_handle.cpu_handle;
+   for (int i = 0; i < ctx->fb.nr_cbufs; ++i) {
+      struct d3d12_surface *surface = d3d12_surface(ctx->fb.cbufs[i]);
+      render_targets[i] = surface->desc_handle.cpu_handle;
+      d3d12_batch_reference_surface(batch, surface);
+   }
    if (ctx->fb.zsbuf) {
-      tmp_desc = d3d12_surface(ctx->fb.zsbuf)->desc_handle.cpu_handle;
+      struct d3d12_surface *surface = d3d12_surface(ctx->fb.zsbuf);
+      tmp_desc = surface->desc_handle.cpu_handle;
+      d3d12_batch_reference_surface(batch, surface);
       depth_desc = &tmp_desc;
    }
    ctx->cmdlist->OMSetRenderTargets(ctx->fb.nr_cbufs, render_targets, FALSE, depth_desc);
    ctx->cmdlist->OMSetStencilRef(ctx->stencil_ref.ref_value[0]);
 
    ctx->cmdlist->IASetPrimitiveTopology(topology(dinfo->mode));
-   ctx->cmdlist->IASetVertexBuffers(0, ctx->num_vbs, ctx->vbvs);
 
+   for (int i = 0; i < ctx->num_vbs; ++i) {
+      if (ctx->vbs[i].buffer.resource)
+         d3d12_batch_reference_resource(batch, d3d12_resource(ctx->vbs[i].buffer.resource));
+   }
+   ctx->cmdlist->IASetVertexBuffers(0, ctx->num_vbs, ctx->vbvs);
 
    for (int i = 0; i < ctx->fb.nr_cbufs; ++i) {
       struct pipe_surface *psurf = ctx->fb.cbufs[i];
@@ -598,6 +614,7 @@ d3d12_draw_vbo(struct pipe_context *pctx,
       ibv.SizeInBytes = res->base.width0 - index_offset;
       ibv.Format = ib_format(dinfo->index_size);
 
+      d3d12_batch_reference_resource(batch, res);
       ctx->cmdlist->IASetIndexBuffer(&ibv);
       ctx->cmdlist->DrawIndexedInstanced(dinfo->count, dinfo->instance_count,
                                          dinfo->start, dinfo->index_bias,

src/gallium/drivers/d3d12/d3d12_query.cpp
@@ -197,12 +197,16 @@ d3d12_begin_query(struct pipe_context *pctx,
 static void
 end_query(struct d3d12_context *ctx, struct d3d12_query *q)
 {
+   struct d3d12_batch *batch = d3d12_current_batch(ctx);
    struct d3d12_resource *res = (struct d3d12_resource *)q->buffer;
    unsigned offset = q->buffer_offset + q->curr_query * sizeof(uint64_t);
 
    ctx->cmdlist->EndQuery(q->query_heap, q->d3d12qtype, q->curr_query);
    ctx->cmdlist->ResolveQueryData(q->query_heap, q->d3d12qtype, q->curr_query,
                                   1, res->res, offset);
+
+   d3d12_batch_reference_object(batch, q->query_heap);
+   d3d12_batch_reference_object(batch, res->res);
 
    assert(q->curr_query < q->num_queries);
    q->curr_query++;

src/gallium/drivers/d3d12/d3d12_resource.cpp
@@ -183,6 +183,7 @@ d3d12_transfer_copy_bufimage(struct d3d12_context *ctx,
                              unsigned stride,
                              bool buf2img)
 {
+   struct d3d12_batch *batch = d3d12_current_batch(ctx);
    ID3D12Device* dev = d3d12_screen(ctx->base.screen)->dev;
    D3D12_TEXTURE_COPY_LOCATION buf_loc = {};
    D3D12_TEXTURE_COPY_LOCATION tex_loc = {};
@@ -234,6 +235,9 @@ d3d12_transfer_copy_bufimage(struct d3d12_context *ctx,
    buf_loc.PlacedFootprint.Footprint.RowPitch = stride;
 
    buf_loc.pResource = staging_res->res;
+
+   d3d12_batch_reference_resource(batch, res);
+   d3d12_batch_reference_resource(batch, staging_res);
 
    ctx->cmdlist->CopyTextureRegion(dst, dst_x, dst_y, dst_z, src, buf2img ? NULL : &src_box);
 
@@ -388,6 +392,7 @@ d3d12_resource_copy_region(struct pipe_context *pctx,
                            const struct pipe_box *psrc_box)
 {
    struct d3d12_context *ctx = d3d12_context(pctx);
+   struct d3d12_batch *batch = d3d12_current_batch(ctx);
    struct d3d12_resource *dst = d3d12_resource(pdst);
    struct d3d12_resource *src = d3d12_resource(psrc);
    D3D12_TEXTURE_COPY_LOCATION src_loc, dst_loc;
@@ -426,6 +431,9 @@ d3d12_resource_copy_region(struct pipe_context *pctx,
    dst_loc.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
    dst_loc.SubresourceIndex = dst_level;
    dst_loc.pResource = dst->res;
+
+   d3d12_batch_reference_resource(batch, src);
+   d3d12_batch_reference_resource(batch, dst);
 
    ctx->cmdlist->CopyTextureRegion(&dst_loc, dstx, dsty, dstz,
                                    &src_loc, &src_box);

commit fc679e202349bf752dea08bb362755d82b4ed65f

d3d12: Destroy blitter before destroying the batches and free zombie_samplers
* The blitter might add a sampler to the sampler zombie list of the
  currently active batch, so it should be destroyed first, to then let
  the zombies be destroyed with the batches.
* Free the memory used by the zombie_samplers array when the batch
  is destroyed

Fixes: 9805bc528f10
   d3d12: Keep track of resources used in a batch

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>

src/gallium/drivers/d3d12/d3d12_batch.cpp
@@ -135,6 +135,7 @@ d3d12_destroy_batch(struct d3d12_context *ctx, struct d3d12_batch *batch)
    _mesa_set_destroy(batch->sampler_views, NULL);
    _mesa_set_destroy(batch->surfaces, NULL);
    _mesa_set_destroy(batch->objects, NULL);
+   util_dynarray_fini(&batch->zombie_samplers);
 }
 
 void

src/gallium/drivers/d3d12/d3d12_context.cpp
@@ -53,12 +53,12 @@ d3d12_context_destroy(struct pipe_context *pctx)
    struct d3d12_context *ctx = d3d12_context(pctx);
    d3d12_validator_destroy(ctx->validation_tools);
 
+   util_blitter_destroy(ctx->blitter);
    d3d12_end_batch(ctx, d3d12_current_batch(ctx));
    for (int i = 0; i < ARRAY_SIZE(ctx->batches); ++i)
       d3d12_destroy_batch(ctx, &ctx->batches[i]);
    ctx->cmdlist->Release();
    ctx->cmdqueue_fence->Release();
-   util_blitter_destroy(ctx->blitter);
    d3d12_descriptor_heap_free(ctx->rtv_heap);
    d3d12_descriptor_heap_free(ctx->dsv_heap);
    d3d12_descriptor_pool_free(ctx->sampler_pool);