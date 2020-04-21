db10d724cb07df5458e79723ce42987fe4e79c78
Jesse Natalie <jenatali@microsoft.com>
2020-04-21 09:28:21 -0700

    microsoft/compiler: Support non-float SRVs

--- src/microsoft/compiler/dxil_module.h
+++ src/microsoft/compiler/dxil_module.h
@@ -260,7 +260,7 @@ const struct dxil_type *
 dxil_module_get_cbuf_ret_type(struct dxil_module *mod, enum overload_type overload);
 
 const struct dxil_type *
-dxil_module_get_res_type(struct dxil_module *m, enum dxil_resource_kind kind);
+dxil_module_get_res_type(struct dxil_module *m, enum dxil_resource_kind kind, enum dxil_component_type comp_type);
 
 const struct dxil_type *
 dxil_module_get_resret_type(struct dxil_module *m, enum overload_type overload);
