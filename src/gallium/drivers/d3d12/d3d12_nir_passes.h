fa0257709ea51f3b073994499f0adf540e2e6628
Erik Faye-Lund <erik.faye-lund@collabora.com>
2020-05-05 18:26:37 +0200

    d3d12: remove bool-load lowering-code
    NIR actually does the right thing for us now, so we no longer need to do
    this.
    
    Reviewed-by: Gert Wollny <gert.wollny@collabora.com>

--- src/gallium/drivers/d3d12/d3d12_nir_passes.h
+++ src/gallium/drivers/d3d12/d3d12_nir_passes.h
@@ -33,9 +33,6 @@ extern "C" {
 struct d3d12_shader;
 
 bool
-d3d12_lower_bool_loads(struct nir_shader *s);
-
-bool
 d3d12_lower_point_sprite(nir_shader *shader,
                          bool sprite_origin_lower_left,
                          unsigned point_coord_enable);
