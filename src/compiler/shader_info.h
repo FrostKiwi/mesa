2aa0a0c2880478fcb1773f67303eec903f4c9e2d
Jesse Natalie <jenatali@microsoft.com>
2020-06-21 14:35:29 -0700

    nir: Add printf cap and intrinsic

--- src/compiler/shader_info.h
+++ src/compiler/shader_info.h
@@ -87,6 +87,7 @@ struct spirv_supported_capabilities {
    bool amd_trinary_minmax;
    bool amd_image_read_write_lod;
    bool amd_shader_explicit_vertex_parameter;
+   bool printf;
 };
 
 typedef struct shader_info {
