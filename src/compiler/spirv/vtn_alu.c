f5879216640356c0d974fcb5e7a1cb02f4376569
Jesse Natalie <jenatali@microsoft.com>
2020-06-23 15:22:44 -0700

    nir/vtn: Support SpvOpIsFinite via fisfinite

--- src/compiler/spirv/vtn_alu.c
+++ src/compiler/spirv/vtn_alu.c
@@ -378,6 +378,7 @@ vtn_nir_alu_op_for_spirv_opcode(struct vtn_builder *b, struct vtn_value *val,
    case SpvOpDPdyCoarse:   return nir_op_fddy_coarse;
 
    case SpvOpIsNormal:     return nir_op_fisnormal;
+   case SpvOpIsFinite:     return nir_op_fisfinite;
 
    default:
       vtn_fail("No NIR equivalent: %u", opcode);
