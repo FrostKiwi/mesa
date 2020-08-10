77eedf2ee08ad037e5b18e0f834d92fc2813e966
Jason Ekstrand <jason@jlekstrand.net>
2020-08-10 18:33:46 -0500

    iris: Add pipe-loader support
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>

---
+++ src/gallium/targets/pipe-loader/pipe_iris.c
@@ -0,0 +1,20 @@
+
+#include "target-helpers/inline_debug_helper.h"
+#include "frontend/drm_driver.h"
+#include "iris/drm/iris_drm_public.h"
+#include "util/driconf.h"
+
+static struct pipe_screen *
+create_screen(int fd, const struct pipe_screen_config *config)
+{
+   struct pipe_screen *screen = iris_drm_screen_create(fd, config);
+
+   return screen ? debug_screen_wrap(screen) : NULL;
+}
+
+static const char *driconf_xml =
+   #include "iris/iris_driinfo.h"
+   ;
+
+PUBLIC
+DRM_DRIVER_DESCRIPTOR("iris", &driconf_xml, create_screen)
