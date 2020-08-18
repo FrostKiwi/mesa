30807547d4bb9ecbc83eddde8b08fea58fbd8c9a
Jesse Natalie <jenatali@microsoft.com>
2020-08-18 11:42:25 -0700

    microsoft/compiler: NIR's fne should be unordered

--- src/microsoft/compiler/nir_to_dxil.c
+++ src/microsoft/compiler/nir_to_dxil.c
@@ -1963,7 +1963,7 @@ emit_alu(struct ntd_context *ctx, nir_alu_instr *alu)
    case nir_op_ilt:  return emit_cmp(ctx, alu, DXIL_ICMP_SLT, src[0], src[1]);
    case nir_op_ult:  return emit_cmp(ctx, alu, DXIL_ICMP_ULT, src[0], src[1]);
    case nir_op_feq:  return emit_cmp(ctx, alu, DXIL_FCMP_OEQ, src[0], src[1]);
-   case nir_op_fne:  return emit_cmp(ctx, alu, DXIL_FCMP_ONE, src[0], src[1]);
+   case nir_op_fne:  return emit_cmp(ctx, alu, DXIL_FCMP_UNE, src[0], src[1]);
    case nir_op_flt:  return emit_cmp(ctx, alu, DXIL_FCMP_OLT, src[0], src[1]);
    case nir_op_fge:  return emit_cmp(ctx, alu, DXIL_FCMP_OGE, src[0], src[1]);
    case nir_op_bcsel: return emit_select(ctx, alu, src[0], src[1], src[2]);
